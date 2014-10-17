da96465bf1f8e2188cab515e4876681f727cd33a
Brian Craft <craft@soe.ucsc.edu>
2014-10-17 12:30:04 -0700

    Rename jing_helper to dom_helper.

--- js/datapages.js
+++ js/datapages.js
@@ -3,13 +3,13 @@
 /*global _: true */
 /*global define: false */
 
-define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data) {
+define(["dom_helper","xenaQuery","data"], function (dom_helper,xenaQuery,data) {
         'use strict';
 
 		var hosts,     /* hosts is the variable holds all hosts*/
 			activeHosts,
 			userHosts,
-			query_string = jing_helper.queryStringToJSON();
+			query_string = dom_helper.queryStringToJSON();
 
         data.sessionStorageInitialize();
 		hosts = JSON.parse(sessionStorage.state).allHosts;  // all hosts
@@ -22,24 +22,24 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 
 		// the short COHORT section
 		function eachCohort (cohortName, hosts, mode) {
-			var node = jing_helper.sectionNode ("cohort"), nodeTitle, hostNode;
+			var node = dom_helper.sectionNode ("cohort"), nodeTitle, hostNode;
 
 			//cohort title
-			nodeTitle = jing_helper.hrefLink (cohortName+" cohort", "?cohort="+encodeURIComponent(cohortName));
+			nodeTitle = dom_helper.hrefLink (cohortName+" cohort", "?cohort="+encodeURIComponent(cohortName));
 
 			if (mode==="multiple") {
-				node.appendChild(jing_helper.elt ("h3", jing_helper.elt("multiple", nodeTitle)));
+				node.appendChild(dom_helper.elt ("h3", dom_helper.elt("multiple", nodeTitle)));
 			}
 			else if (mode ==="single") {
-				node.appendChild(jing_helper.elt ("h2", cohortName+ " cohort"));
+				node.appendChild(dom_helper.elt ("h2", cohortName+ " cohort"));
 			}
 
 			// samples: N node
-			node.appendChild(jing_helper.labelValueNode("samples:",cohortName+"sampleN"));
-			jing_helper.updateDOM_xenaCohort_sampleN (cohortName+"sampleN", hosts, cohortName);
+			node.appendChild(dom_helper.labelValueNode("samples:",cohortName+"sampleN"));
+			dom_helper.updateDOM_xenaCohort_sampleN (cohortName+"sampleN", hosts, cohortName);
 
 			// update
-			node.appendChild(jing_helper.valueNode(cohortName+"Update"));
+			node.appendChild(dom_helper.valueNode(cohortName+"Update"));
 			hosts.forEach(function(host){
 				xenaQuery.dataset_list([host], cohortName).subscribe(
 					function (s){
@@ -54,7 +54,7 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 						if (version){
 							node = document.getElementById(cohortName+"Update");
 							if (node.children.length===0){
-								node.appendChild(jing_helper.elt ("label","updated on:"));
+								node.appendChild(dom_helper.elt ("label","updated on:"));
 								node.appendChild(document.createTextNode(version));
 							}
 							else{
@@ -65,17 +65,17 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 			});
 
 			// host: xxx
-			hostNode =jing_helper.valueNode(cohortName+"Hosts");
+			hostNode =dom_helper.valueNode(cohortName+"Hosts");
 			hosts.forEach(function(host){
 				xenaQuery.all_cohorts(host).subscribe(function(s){
 				if (s.indexOf(cohortName)!==-1){
 					if (hostNode.children.length===0){
-						hostNode.appendChild(jing_helper.elt("label","hosts:"));
-						hostNode.appendChild(jing_helper.hrefLink(host,"?host="+host));
+						hostNode.appendChild(dom_helper.elt("label","hosts:"));
+						hostNode.appendChild(dom_helper.hrefLink(host,"?host="+host));
 					}
 					else{
 						hostNode.appendChild(document.createTextNode("; "));
-						hostNode.appendChild(jing_helper.hrefLink(host,"?host="+host));
+						hostNode.appendChild(dom_helper.hrefLink(host,"?host="+host));
 					}
 				}
 				});
@@ -150,7 +150,7 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 					});
 
 					// dataType section
-					var nodeDataType= jing_helper.sectionNode("dataType");
+					var nodeDataType= dom_helper.sectionNode("dataType");
 
 					var keys = Object.keys(dataType).sort(),
 						i, type, displayType,
@@ -161,31 +161,31 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 						if (type === "undefined") {
 							displayType ="others";
 						}
-						nodeDataType.appendChild(jing_helper.elt("header", displayType));
-						listNode = jing_helper.elt("ul");
+						nodeDataType.appendChild(dom_helper.elt("header", displayType));
+						listNode = dom_helper.elt("ul");
 						dataType[type].sort().forEach( function(fullname){
 							// name
-							var datasetNode = jing_helper.elt("li",
-								jing_helper.hrefLink (dataLabel[fullname], "?dataset="+dataName[fullname]+"&host="+dataHost[fullname]));
+							var datasetNode = dom_helper.elt("li",
+								dom_helper.hrefLink (dataLabel[fullname], "?dataset="+dataName[fullname]+"&host="+dataHost[fullname]));
 
 							// samples: N
-							datasetNode.appendChild(jing_helper.valueNode(fullname+"sampleN"));
+							datasetNode.appendChild(dom_helper.valueNode(fullname+"sampleN"));
 							xenaQuery.dataset_samples(dataHost[fullname],dataName[fullname]).subscribe( function(s) {
 								document.getElementById(fullname+"sampleN").
-								appendChild(jing_helper.elt("label",document.createTextNode("(n="+s.length.toLocaleString()+")")));
+								appendChild(dom_helper.elt("label",document.createTextNode("(n="+s.length.toLocaleString()+")")));
 							});
 
 							// host if there are multiple hosts
 							if ( _.uniq(_.values(dataHost)).length>1){
-								datasetNode.appendChild(jing_helper.elt (
-									"result2", jing_helper.hrefLink(dataHost[fullname],"?host="+dataHost[name])));
+								datasetNode.appendChild(dom_helper.elt (
+									"result2", dom_helper.hrefLink(dataHost[fullname],"?host="+dataHost[name])));
 							}
 
 							//dataset description
 							if (dataDescription[fullname]){
-								var descriptionNode =jing_helper.elt("div");
+								var descriptionNode =dom_helper.elt("div");
 								descriptionNode.setAttribute("class", "line-clamp");
-								descriptionNode.appendChild(jing_helper.elt("summary", jing_helper.stripHTML(dataDescription[fullname])));
+								descriptionNode.appendChild(dom_helper.elt("summary", dom_helper.stripHTML(dataDescription[fullname])));
 
 								datasetNode.appendChild(descriptionNode);
 							}
@@ -197,25 +197,25 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 					document.body.appendChild(nodeDataType);
 
 					// samples section
-					var nodeSamples= jing_helper.elt ("section");
+					var nodeSamples= dom_helper.elt ("section");
 					nodeSamples.setAttribute("id", "samples");
-					nodeSamples.appendChild(jing_helper.elt("header","Samples"));
+					nodeSamples.appendChild(dom_helper.elt("header","Samples"));
 					document.body.appendChild(nodeSamples);
 
 					//////////////kind of hacky
 					xenaQuery.all_samples(_.uniq(_.values(dataHost))[0], cohortName).subscribe(
 						function(s) {
-							var listNode = jing_helper.elt("ul");
+							var listNode = dom_helper.elt("ul");
 							s.slice(0,10).forEach(function(sample){
-								listNode.appendChild(jing_helper.elt(
-									"li", jing_helper.hrefLink (sample, "?sample="+sample+"&cohort="+cohortName)));
+								listNode.appendChild(dom_helper.elt(
+									"li", dom_helper.hrefLink (sample, "?sample="+sample+"&cohort="+cohortName)));
 							});
 							if (s.length>10) {
-								listNode.appendChild(jing_helper.elt("sampleid","..."));
+								listNode.appendChild(dom_helper.elt("sampleid","..."));
 							}
 							nodeSamples.appendChild(listNode);
 						});
-					document.body.appendChild(jing_helper.elt("br"));
+					document.body.appendChild(dom_helper.elt("br"));
 				});
 		}
 
@@ -239,11 +239,11 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 				urls;
 
 			if (description) {
-				description = jing_helper.stripScripts(description);
+				description = dom_helper.stripScripts(description);
 			}
 
 			if (wrangling_procedure) {
-				wrangling_procedure = jing_helper.stripScripts(wrangling_procedure);
+				wrangling_procedure = dom_helper.stripScripts(wrangling_procedure);
 			}
 
 			if (url) {
@@ -251,60 +251,60 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 			}
 
 			// layout
-			var sectionNode = jing_helper.sectionNode("dataset");
+			var sectionNode = dom_helper.sectionNode("dataset");
 
 			// dataset title
-			sectionNode.appendChild(jing_helper.elt("h2",label));
+			sectionNode.appendChild(dom_helper.elt("h2",label));
 
 			// long title
 			if (longTitle) {
 				sectionNode.appendChild(document.createTextNode(longTitle));
-				sectionNode.appendChild(jing_helper.elt("br"));
+				sectionNode.appendChild(dom_helper.elt("br"));
 			}
 			// cohort:xxx
-			sectionNode.appendChild(jing_helper.elt("label","chort:"));
-			sectionNode.appendChild(jing_helper.elt("result",jing_helper.hrefLink(cohort,"?cohort="+cohort)));
+			sectionNode.appendChild(dom_helper.elt("label","chort:"));
+			sectionNode.appendChild(dom_helper.elt("result",dom_helper.hrefLink(cohort,"?cohort="+cohort)));
 
 			// samples: n
-			sectionNode.appendChild(jing_helper.labelValueNode("samples:", dataset+"SampleN"));
-			jing_helper.updataDOM_xenaDataSet_sampleN (dataset+"SampleN", host, name);
+			sectionNode.appendChild(dom_helper.labelValueNode("samples:", dataset+"SampleN"));
+			dom_helper.updataDOM_xenaDataSet_sampleN (dataset+"SampleN", host, name);
 			// update on: xxx
 			if (version){
-				sectionNode.appendChild(jing_helper.elt("label","version:"));
-				sectionNode.appendChild(jing_helper.elt("result",version));
+				sectionNode.appendChild(dom_helper.elt("label","version:"));
+				sectionNode.appendChild(dom_helper.elt("result",version));
 			}
 			//  host: host
-			sectionNode.appendChild(jing_helper.elt("label","host:"));
-			sectionNode.appendChild(jing_helper.elt("result",jing_helper.hrefLink(host,"?host="+host)));
-			sectionNode.appendChild(jing_helper.elt("br"));
+			sectionNode.appendChild(dom_helper.elt("label","host:"));
+			sectionNode.appendChild(dom_helper.elt("result",dom_helper.hrefLink(host,"?host="+host)));
+			sectionNode.appendChild(dom_helper.elt("br"));
 
 			// assembly
 			if(assembly || platform) {
 				if (assembly) {
-					sectionNode.appendChild(jing_helper.elt("label","assembly:"));
-					sectionNode.appendChild(jing_helper.elt("result",assembly));
+					sectionNode.appendChild(dom_helper.elt("label","assembly:"));
+					sectionNode.appendChild(dom_helper.elt("result",assembly));
 				}
 			//platform
 				if (platform){
-					sectionNode.appendChild(jing_helper.elt("label","platform:"));
-					sectionNode.appendChild(jing_helper.elt("result",platform));
+					sectionNode.appendChild(dom_helper.elt("label","platform:"));
+					sectionNode.appendChild(dom_helper.elt("result",platform));
 				}
-				sectionNode.appendChild(jing_helper.elt("br"));
+				sectionNode.appendChild(dom_helper.elt("br"));
 			}
 			// Downlaod
 			if (host === "https://genome-cancer.ucsc.edu/proj/public/xena"){
-				sectionNode.appendChild(jing_helper.elt("label","Download:"));
+				sectionNode.appendChild(dom_helper.elt("label","Download:"));
 				var array = name.split("/"),
 					link = "https://genome-cancer.ucsc.edu/download/public/xena/"+	array.slice(1,array.length).join("/");
-				sectionNode.appendChild(jing_helper.elt("result", jing_helper.hrefLink(link,link)));
-				sectionNode.appendChild(jing_helper.elt("br"));
+				sectionNode.appendChild(dom_helper.elt("result", dom_helper.hrefLink(link,link)));
+				sectionNode.appendChild(dom_helper.elt("br"));
 			}
 
 			//description
 			if (description) {
-				sectionNode.appendChild(jing_helper.sectionNode("description"));
+				sectionNode.appendChild(dom_helper.sectionNode("description"));
 
-				var tmpNode = jing_helper.elt("description");
+				var tmpNode = dom_helper.elt("description");
 				tmpNode.innerHTML= description;
 
 				sectionNode.appendChild(tmpNode);
@@ -312,63 +312,63 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 			document.body.appendChild(sectionNode);
 
 			// others
-			sectionNode= jing_helper.sectionNode("others");
+			sectionNode= dom_helper.sectionNode("others");
 			if (articletitle){
-				sectionNode.appendChild(jing_helper.elt("label","Publication:"));
-				sectionNode.appendChild(jing_helper.elt("result", articletitle));
-				sectionNode.appendChild(jing_helper.elt("br"));
+				sectionNode.appendChild(dom_helper.elt("label","Publication:"));
+				sectionNode.appendChild(dom_helper.elt("result", articletitle));
+				sectionNode.appendChild(dom_helper.elt("br"));
 			}
 			if (citation){
-				sectionNode.appendChild(jing_helper.elt("label","Citation:"));
-				sectionNode.appendChild(jing_helper.elt("result", citation));
-				sectionNode.appendChild(jing_helper.elt("br"));
+				sectionNode.appendChild(dom_helper.elt("label","Citation:"));
+				sectionNode.appendChild(dom_helper.elt("result", citation));
+				sectionNode.appendChild(dom_helper.elt("br"));
 			}
 			if (pmid && (typeof pmid)=== "number") {
-				sectionNode.appendChild(jing_helper.elt("label","PMID:"));
+				sectionNode.appendChild(dom_helper.elt("label","PMID:"));
 				sectionNode.appendChild(
-					jing_helper.elt("result", jing_helper.hrefLink (
+					dom_helper.elt("result", dom_helper.hrefLink (
 						pmid.toString(), "http://www.ncbi.nlm.nih.gov/pubmed/?term="+pmid.toString())));
-				sectionNode.appendChild(jing_helper.elt("br"));
+				sectionNode.appendChild(dom_helper.elt("br"));
 			}
 			if (urls){
 				urls.forEach(function(url){
-					sectionNode.appendChild(jing_helper.elt("label","Raw data:"));
-					sectionNode.appendChild(jing_helper.elt("result", jing_helper.hrefLink (url, url)));
-					sectionNode.appendChild(jing_helper.elt("br"));
+					sectionNode.appendChild(dom_helper.elt("label","Raw data:"));
+					sectionNode.appendChild(dom_helper.elt("result", dom_helper.hrefLink (url, url)));
+					sectionNode.appendChild(dom_helper.elt("br"));
 				});
 			}
 			if (sectionNode.children.length>0) {
-				sectionNode.appendChild(jing_helper.elt("br"));
+				sectionNode.appendChild(dom_helper.elt("br"));
 			}
 
 			if (wrangling_procedure){
-				sectionNode.appendChild(jing_helper.elt("label","Wrangling:"));
+				sectionNode.appendChild(dom_helper.elt("label","Wrangling:"));
 
-				var tmpNode = jing_helper.elt("result");
+				var tmpNode = dom_helper.elt("result");
 				tmpNode.innerHTML= wrangling_procedure;
 
 				sectionNode.appendChild(tmpNode);
-				sectionNode.appendChild(jing_helper.elt("br"));
+				sectionNode.appendChild(dom_helper.elt("br"));
 			}
 			if (sectionNode.children.length>0) {
 				document.body.appendChild(sectionNode);
 			}
 
-			sectionNode= jing_helper.sectionNode("others");
-			sectionNode.appendChild(jing_helper.elt("header","The dataset has the following variables:"));
+			sectionNode= dom_helper.sectionNode("others");
+			sectionNode.appendChild(dom_helper.elt("header","The dataset has the following variables:"));
 			xenaQuery.dataset_field_examples(host,name).subscribe(function(s){
 					s.sort();
 					if (type==="genomicMatrix") {
 						s=s.slice(0,10);
 					}
-					var listNode=jing_helper.elt("ul");
+					var listNode=dom_helper.elt("ul");
 					s.forEach(function(o) {
 						if (o.hasOwnProperty("name")) {
-							listNode.appendChild(jing_helper.elt("identifiers",jing_helper.elt("li",o.name)));
+							listNode.appendChild(dom_helper.elt("identifiers",dom_helper.elt("li",o.name)));
 						}
 					});
 					if ((type==="genomicMatrix" && s.length===10) || (s.length===100)) {
-						listNode.appendChild(jing_helper.elt("identifiers1","..."));
+						listNode.appendChild(dom_helper.elt("identifiers1","..."));
 					}
 					sectionNode.appendChild(listNode);
 				});
@@ -378,13 +378,13 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 		// build single SAMPLE page
 		function samplePage(sample,cohort) {
 			// layout
-			var sectionNode = jing_helper.sectionNode("dataset");
+			var sectionNode = dom_helper.sectionNode("dataset");
 
 			// sample title
-			var nodeTitle = jing_helper.hrefLink (sample, "?sample="+sample+"&cohort="+cohort);
-			sectionNode.appendChild(jing_helper.elt ("h3", nodeTitle));
-			sectionNode.appendChild(jing_helper.elt("label", "cohort:"));
-			sectionNode.appendChild(jing_helper.elt("result", jing_helper.hrefLink (cohort, "?&cohort="+cohort)));
+			var nodeTitle = dom_helper.hrefLink (sample, "?sample="+sample+"&cohort="+cohort);
+			sectionNode.appendChild(dom_helper.elt ("h3", nodeTitle));
+			sectionNode.appendChild(dom_helper.elt("label", "cohort:"));
+			sectionNode.appendChild(dom_helper.elt("result", dom_helper.hrefLink (cohort, "?&cohort="+cohort)));
 
 			document.body.appendChild(sectionNode);
 		}
@@ -400,11 +400,11 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 			host = decodeURIComponent(query_string.host);
 
 			// host title
-			var node = jing_helper.sectionNode("host");
-			var tmpNode = jing_helper.hrefLink(host+" (connecting)","index.html?host="+host);
+			var node = dom_helper.sectionNode("host");
+			var tmpNode = dom_helper.hrefLink(host+" (connecting)","index.html?host="+host);
 			tmpNode.setAttribute("id","status"+host);
 
-			node.appendChild(jing_helper.elt("h3",tmpNode));
+			node.appendChild(dom_helper.elt("h3",tmpNode));
 			data.updateHostStatus (host);
 			document.body.appendChild(node);
 
@@ -441,32 +441,32 @@ define(["jing_helper","xenaQuery","data"], function (jing_helper,xenaQuery,data)
 		}
 
 		else {
-			var container = jing_helper.elt("div");
+			var container = dom_helper.elt("div");
 			container.setAttribute("id","content-container");
 
 			//checkbox sidebar
-			var sideNode = jing_helper.elt("div");
+			var sideNode = dom_helper.elt("div");
 			sideNode.setAttribute("id","sidebar");
 			container.appendChild(sideNode);
 
-			var checkNode = jing_helper.sectionNode("sidehub");
+			var checkNode = dom_helper.sectionNode("sidehub");
 
-			checkNode.appendChild(jing_helper.elt("h2",jing_helper.hrefLink("Current Data Hubs","hub.html")));
-			checkNode.appendChild(jing_helper.elt("br"));
+			checkNode.appendChild(dom_helper.elt("h2",dom_helper.hrefLink("Current Data Hubs","hub.html")));
+			checkNode.appendChild(dom_helper.elt("br"));
 
 			userHosts.forEach(function(host){
-				var	tmpNode = jing_helper.elt("result2", jing_helper.hrefLink(host+" (connecting)","index.html?host="+host));
+				var	tmpNode = dom_helper.elt("result2", dom_helper.hrefLink(host+" (connecting)","index.html?host="+host));
 				tmpNode.setAttribute("id","status"+host);
-				checkNode.appendChild(jing_helper.elt("h4", tmpNode));
-				checkNode.appendChild(jing_helper.elt("br"));
+				checkNode.appendChild(dom_helper.elt("h4", tmpNode));
+				checkNode.appendChild(dom_helper.elt("br"));
 			});
 
 		    sideNode.appendChild(checkNode);
 
 		    //cohort list page
-			var mainNode = jing_helper.elt("div");
+			var mainNode = dom_helper.elt("div");
 			mainNode.setAttribute("id","main");
-			mainNode.appendChild(jing_helper.elt("h2","Cohorts"));
+			mainNode.appendChild(dom_helper.elt("h2","Cohorts"));
 
 			cohortListPage(_.intersection(activeHosts, userHosts), mainNode);
 	        container.appendChild(mainNode);
--- js/hub.js
+++ js/hub.js
@@ -1,34 +1,34 @@
 /*jslint browser: true,  nomen: true*/
 /*global define: false */
 
-define(["jing_helper", "data", "xenaQuery"], function (jing_helper, data, xenaQuery) {
+define(["dom_helper", "data", "xenaQuery"], function (dom_helper, data, xenaQuery) {
 	'use strict';
 
 	function newHubNode(host) {
 		//build checkbox
 		var checkbox = data.hostCheckBox(host),
-			tmpNode = jing_helper.elt("result2", jing_helper.hrefLink(host + " (connecting)","index.html?host=" + host));
+			tmpNode = dom_helper.elt("result2", dom_helper.hrefLink(host + " (connecting)","index.html?host=" + host));
 		tmpNode.setAttribute("id","status" + host);
-		return jing_helper.elt("h3", checkbox," ",tmpNode);
+		return dom_helper.elt("h3", checkbox," ",tmpNode);
 	}
 
 	data.sessionStorageInitialize();
 	var hosts = JSON.parse(sessionStorage.state).allHosts,
-	node = jing_helper.sectionNode("hub"),
+	node = dom_helper.sectionNode("hub"),
 		newText;
 
-	node.appendChild(jing_helper.elt("h2", "Data Hubs"));
-	node.appendChild(jing_helper.elt("br"));
+	node.appendChild(dom_helper.elt("h2", "Data Hubs"));
+	node.appendChild(dom_helper.elt("br"));
 	hosts.forEach(function(host) {
 		node.appendChild(newHubNode(host));
-		node.appendChild(jing_helper.elt("br"));
+		node.appendChild(dom_helper.elt("br"));
 		data.updateHostStatus(host);
 	});
 
 	newText = document.createElement("INPUT");
 	newText.setAttribute("class","tb5");
 	newText.setAttribute("id","textHub");
-	node.appendChild(jing_helper.elt("italic","Add  "));
+	node.appendChild(dom_helper.elt("italic","Add  "));
 	node.appendChild(newText);
 	document.body.appendChild(node);
 
@@ -54,7 +54,7 @@ define(["jing_helper", "data", "xenaQuery"], function (jing_helper, data, xenaQu
 				return;
 			}
 			node.parentNode.insertBefore(newHubNode(host), node.previousSibling);
-			node.parentNode.insertBefore(jing_helper.elt("br"), node.previousSibling);
+			node.parentNode.insertBefore(dom_helper.elt("br"), node.previousSibling);
 			hosts.push(host);
 			node.value ="";
 			data.updateHostStatus(host);
