743ef59b50aaa4a53626a4c8e0cb8aabe5f034bc
Teresa Swatloski <swat@soe.ucsc.edu>
2014-11-07 14:59:54 -0800

    ported columnLabel & fieldLabel to columnEdit rx

--- js/columnUi.js
+++ js/columnUi.js
@@ -220,11 +220,6 @@ define(['haml!haml/columnUi', 'haml!haml/columnUiSelect', 'haml!haml/tupleDispla
 			this.headerPlotHeight = options.headerPlotHeight;
 			this.horizontalMargin = options.horizontalMargin.toString() + 'px';
 
-			// initialize the root of this _column in the state tree
-			this.xenaCursor.update(function (state) {
-				return _.assoc(state, '_column', _.assoc(state._column, self.id, {}));
-			});
-
 			if (options.ws) {
 				this.render(options);
 			}
