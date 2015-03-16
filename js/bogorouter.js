e500c9690f03fc85c8f295fb4b2120153f9698a9
Brian Craft <craft@soe.ucsc.edu>
2015-03-15 18:04:27 -0700

    Fix require baseurl.

--- js/bogorouter.js
+++ js/bogorouter.js
@@ -1,5 +1,7 @@
-/*global document: false, require: false */
+/*global document: false, require: false, __webpack_public_path__: true */
 'use strict';
+var config = require('config');
+__webpack_public_path__ = config.baseurl;
 if (document.location.pathname.match(/datapages\/$/)) {
 	require.ensure(['datapages'], function () {
 		require(['datapages']);
