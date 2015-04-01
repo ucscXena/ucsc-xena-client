f9db6bf0b51251525042b9ba57d154142440e649
Jing <jzhu1@Jingchun-Zhus-MacBook-Pro.local>
2015-03-31 22:40:00 -0700

    add galaxy xena link

--- js/nav.js
+++ js/nav.js
@@ -7,6 +7,7 @@ var links = [
 	{href: "../datapages/", label: "Explore Data"},
 	{href: "../heatmap/", label: "Visualization"},
 	{href: "../hub/", label: "Data Hubs"},
+  {href: "https://galaxyxena.soe.ucsc.edu", label: "Galaxy Xena"},
 	{href: "https://genome-cancer.ucsc.edu/download/public/get-xena/index.html", label: "Local Xena"},
 	{href:"https://docs.google.com/a/soe.ucsc.edu/document/d/1CIWj6L8LAaHFmLek3yrbrjFKRm_l3Sy73lJ4wY-WM8Y", label: "Help"}
 ];
