0d6bdc70a44fc31e190abb4978ecd10ba3afd1b4
Brian Craft <craft@soe.ucsc.edu>
2015-06-08 12:30:16 -0700

    Fix offset when zooming.

--- js/layoutPlot.js
+++ js/layoutPlot.js
@@ -13,14 +13,17 @@ function flopIf(reversed, start, end) {
 var halfOpen = ([a, b]) => [a - 1, b];
 
 function pxTransformEach(layout, fn) {
-	var {screen, chrom, reversed} = layout;
-	var offset = _.get_in(layout, ['zoom', 'index']);
+	var {screen, chrom, reversed, pxLen, baseLen} = layout;
+	var index = _.get_in(layout, ['zoom', 'index']);
+	// XXX This is wrong if there are spaces between layout elements.
+	var offset =  pxLen / baseLen * index;
 
 	_.each(chrom, (pos, i) => {
 		var [start, end] = pos;
 		var [sstart, send] = screen[i];
 		var flop = flopIf(reversed, start, end);
-		var toPx = x => round(sstart + (x - start + 1 - offset) * (send - sstart + 1) / (end - start + 1));
+		// XXX Why round?
+		var toPx = x => round(sstart + (x - start + 1) * (send - sstart + 1) / (end - start + 1) - offset);
 		var clip = ([s, e]) => [max(s, start), min(e, end)];
 		var intvlToPx = i => _.map(halfOpen(clip(flop(i))), toPx);
 
