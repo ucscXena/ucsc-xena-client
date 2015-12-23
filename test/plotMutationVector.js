e1c9ebc40927e1420b2885918df4c86aedb489a0
Brian Craft <bcboy@thecraftstudio.com>
2015-12-22 16:10:41 -0800

    Update test.

---
+++ test/plotMutationVector.js
@@ -0,0 +1,69 @@
+/*global describe: false, it: false, require: false */
+'use strict';
+//var _ = require('../js/underscore_ext');
+//var assert = require('assert');
+var {column} = require('../js/columnWidgets');
+require('../js/plotMutationVector');
+
+const React = require('react/addons');
+const TestUtils = React.addons.TestUtils;
+const {renderIntoDocument} = TestUtils;
+var {index} = require('static-interval-tree');
+
+var basicMutationColumn = {
+	column: {
+		columnLabel: {user: 'foo', default: 'foo'},
+		fieldLabel: {user: 'foo', default: 'foo'},
+		sFeature: 'impact',
+		width: 20,
+		dataType: 'mutationVector',
+		fields: ['TP53'],
+		layout: {
+			chrom: [[1000, 1200], [1300, 1500]],
+			screen: [[0, 100], [110, 200]],
+			reversed: false
+		}
+	},
+	samples: ['a', 'b', 'c', 'd'],
+	zoom: {
+		height: 100,
+		index: 0,
+		count: 2
+	},
+	index: {
+		byPosition: index([
+			{start: 1000, end: 1010, chr: 'chr10', sample: 'a'},
+			{start: 2000, end: 2010, chr: 'chr10', sample: 'a'},
+			{start: 1000, end: 1010, chr: 'chr10', sample: 'b'}
+			]),
+		bySample: { // should be in 'display'
+			'a': [
+				{start: 1000, end: 1010, chr: 'chr10', sample: 'a'},
+				{start: 2000, end: 2010, chr: 'chr10', sample: 'a'}
+				],
+			'b': [
+				{start: 1000, end: 1010, chr: 'chr10', sample: 'b'}
+				],
+			'c': []
+		}
+	},
+	data: {
+		refGene: {
+			'TP53': {
+				cdsStart: 900,
+				cdsEnd: 3000
+			}
+		},
+		req: {
+		}
+	}
+};
+
+
+describe('plotMutationVector', function () {
+    describe('#column', function () {
+        it('should render mutationVector', function() {
+			renderIntoDocument(column(basicMutationColumn));
+        });
+	});
+});
