6a48e3eaaa02b26d43a53ea65a6ee89e3da118c4
Brian Craft <bcboy@thecraftstudio.com>
2015-12-22 16:45:11 -0800

    update tests.

--- test/all.js
+++ test/all.js
@@ -1,9 +1,13 @@
 /*global require: false */
 "use strict";
 require('./underscore_ext');
-require('./mutationVector');
+// This needs refactor. Might want to return mutationVector methods in exports,
+// and call widget.*.add elsewhere, so we can test the methods w/o widgets.
+//require('./mutationVector');
 require('./exonLayout');
 require('./refGeneExons');
+require('./plotDenseMatrix');
+require('./plotMutationVector');
 require('./lenses/lens');
 require('./heatmapColors');
 require('./scale');
