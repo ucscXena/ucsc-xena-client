372bb4d2bb291982e18c9920c93196af2531838d
Brian Craft <bcboy@thecraftstudio.com>
2015-12-22 16:11:37 -0800

    update test.

---
+++ test/plotDenseMatrix.js
@@ -0,0 +1,48 @@
+/*global describe: false, it: false, require: false */
+'use strict';
+var _ = require('../js/underscore_ext');
+//var assert = require('assert');
+var {column} = require('../js/columnWidgets');
+require('../js/plotDenseMatrix');
+
+const React = require('react/addons');
+const TestUtils = React.addons.TestUtils;
+const {renderIntoDocument} = TestUtils;
+
+var basicDenseColumn = {
+	column: {
+		columnLabel: {user: 'foo', default: 'foo'},
+		fieldLabel: {user: 'foo', default: 'foo'},
+		width: 20,
+		dataType: 'probeMatrix',
+		fields: ['TP53'],
+		heatmap: [[1, 2]],
+		colors: [['float-thresh', 'red', 'white', 'blue', -10, -1, 1, 10]]
+	},
+	samples: ['a', 'b'],
+	zoom: {
+		height: 100,
+		index: 0,
+		count: 2
+	},
+	data: {
+		req: {}
+	}
+};
+
+
+describe('plotDenseMatrix', function () {
+    describe('#column', function () {
+        it('should render probeMatrix', function() {
+			renderIntoDocument(column(basicDenseColumn));
+        });
+        it('should render geneMatrix', function() {
+			renderIntoDocument(
+				column(_.assocIn(basicDenseColumn, ['column', 'dataType', 'geneMatrix'])));
+        });
+        it('should render geneProbesMatrix', function() {
+			renderIntoDocument(
+				column(_.assocIn(basicDenseColumn, ['column', 'dataType', 'geneProbesMatrix'])));
+        });
+	});
+});
