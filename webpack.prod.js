a6c10b88059248f2b79a638d9c4c078a0c69bae1
Brian Craft <bcboy@thecraftstudio.com>
2016-03-01 13:29:04 -0800

    Fix production env for babel.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,17 @@
-/*global require: false, module: false */
+/*global require: false, module: false, process: false */
 'use strict';
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
+process.env.NODE_ENV = '"production"';         // * Not sure why needed both here & in a plugin.
+                                               // Without this babel will not see it.
 config.output.filename = "[name].[chunkhash].js";
 config.output.chunkFilename = "[chunkhash].bundle.js";
 config.plugins = config.plugins.concat([
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(true),
 	new webpack.DefinePlugin({
-		"process.env": {
-			NODE_ENV: '"production"' // disable reactjs run-time checks, docs, etc.
-		}
+		"process.env.NODE_ENV": '"production"' // * see above.
 	})
 ]);
 
