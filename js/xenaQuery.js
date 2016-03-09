a84b81d9d0a0a05b6d164794804051830403472a
Brian Craft <bcboy@thecraftstudio.com>
2016-03-09 13:10:42 -0800

    Slightly simpler query for sparse data.

--- js/xenaQuery.js
+++ js/xenaQuery.js
@@ -250,19 +250,21 @@ define(['rx-dom', 'underscore_ext', 'rx.binding'], function (Rx, _) {
 
 	// XXX Can't rewrite :samples until we can do 'distinct', or 'keys' for category fields, on the server.
 	function sparse_data_string(dataset, samples, gene) {
-		return `(let [getfield (fn [field]\n` +
-		       `                 (:id (car (query {:select [:field.id]\n` +
-		       `                                   :from [:dataset]\n` +
-		       `                                   :join [:field [:= :dataset.id :field.dataset_id]]\n` +
-		       `                                   :where [:and [:= :field.name field] [:= :dataset.name ` + quote(dataset) + `]]}))))\n` +
-		       `      sampleID (getfield "sampleID")]\n` +
-		       `  {:samples (map :value (query {:select [:value]\n` +
-		       `                                :from [:field]\n` +
-		       `                                :join [:code [:= :field.id :field_id]]\n` +
-		       `                                :where [:and [:in :value ${arrayfmt(samples)}][:= :field_id sampleID]]}))\n` +
-		       `   :rows (xena-query {:select ["ref" "alt" "effect" "dna-vaf" "rna-vaf" "amino-acid" "genes" "sampleID" "position"]\n` +
-		       `                      :from [${quote(dataset)}]\n` +
-		       `                      :where [:and [:in :any "genes" [${quote(gene)}]] [:in "sampleID" ${arrayfmt(samples)}]]})})`;
+		return `(let [samples ${arrayfmt(samples)}\n` +
+		       `      dataset ${quote(dataset)}\n` +
+		       `      gene ${quote(gene)}\n` +
+		       '      getfield (fn [field]\n' +
+		       '                 (:id (car (query {:select [:field.id]\n' +
+		       '                                   :from [:dataset]\n' +
+		       '                                   :join [:field [:= :dataset.id :field.dataset_id]]\n' +
+		       '                                   :where [:and [:= :field.name field] [:= :dataset.name dataset]]}))))\n' +
+		       '      sampleID (getfield "sampleID")]\n' +
+		       '  {:samples (map :value (query {:select [:value]\n' +
+		       '                                :from [:code]\n' +
+		       '                                :where [:and [:in :value samples][:= :field_id sampleID]]}))\n' +
+		       '   :rows (xena-query {:select ["ref" "alt" "effect" "dna-vaf" "rna-vaf" "amino-acid" "genes" "sampleID" "position"]\n' +
+		       '                      :from [dataset]\n' +
+		       '                      :where [:and [:in :any "genes" [gene]] [:in "sampleID" samples]]})})';
 	}
 
 	function sparse_data_example_string(dataset, count) {
