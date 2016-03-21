35ba77e0b4a5a37da4d627025f936d093f1366d1
Albert Chang <albertwchang@eduroam-169-233-241-101.ucsc.edu>
2016-03-21 10:09:18 -0700

    Refactored DatasetSelect to use new business rules for presenting dataset options; - Using 'dataSubType' for menu title, setup menus to go blue when being perused - Changed: menu highlighted green when selection is made, but not committed - Fixed: DatasetSelect appropriately shows all dropdown groups vs. committed selection; added glyph icons for navigation buttons; changed button placement and visual configuration - Changed: 'splice' replaced with 'editor', fixed: drop-down menu in Phenotype Editor show default 'Select...' - Changed: reverted back to having an object represent header for each drop down menu; added: checkmark icon for chosen selections within a drop-down menu - Fixed: nav breadcrumbs; changed: reversed 'blacksheep' designation for each section - Fixed: setup Phenotype as a stand-alone link instead of a drop-down menu - Fixed: local hub was not showing up as 1st entry - Fixed: 'View' drop down menu not updating after selection - Fixed: initial browser startup was showing red error screen

--- js/views/DatasetSelect.js
+++ js/views/DatasetSelect.js
@@ -1,6 +1,7 @@
 /*global require: false, module: false */
 'use strict';
 var React = require('react');
+var ButtonGroup = require('react-bootstrap/lib/ButtonGroup');
 var Select = require('./Select');
 var _ = require('../underscore_ext');
 var xenaQuery = require('../xenaQuery');
@@ -9,51 +10,71 @@ var {deepPureRenderMixin} = require('../react-utils');
 // group header for a server
 var header = s => xenaQuery.server_url(s);
 
-var ignored = ['probeMap', 'genePredExt', 'probemap', 'sampleMap', 'genomicSegment'];
-var notIgnored = ds => !_.contains(ignored, ds.type);
+const ignored = ['probeMap', 'genePredExt', 'probemap', 'sampleMap', 'genomicSegment'];
+const exceptions = {
+	clinicalMatrix: 'phenotype',
+	mutationVector: null,
+	genomicMatrix: null
+};
+
 var loaded = ds => ds.status === 'loaded';
+//var filterDatasets = list => list.filter(ds => notIgnored(ds) && loaded(ds));
+//var sortByLabel = list => _.sortBy(list, el => el.label.toLowerCase());
 
-var filterDatasets = list => list.filter(ds => notIgnored(ds) && loaded(ds));
-var sortByLabel = list => _.sortBy(list, el => el.label.toLowerCase());
+function groupsFromDatasets(datasets, localHubUrl) {
+	let dsGroups = {local: {}, remote: {}},
+		localGroupKey = 'Your local hub';
+	_.each(datasets, (ds, key) => {
+		let server = 'remote';
+		if (loaded(ds)) {
+			let group = '';
+			if (JSON.parse(key).host === localHubUrl) {
+				group = localGroupKey;
+				server = 'local';
+			} else {
+				group = exceptions[ds.type] || ds.dataSubType;
+			}
 
-<<<<<<< 34ca75fcf82b2d55a69e2abac0fa9cbc9e73cb4b
-function optsFromDatasets(dataSubTypes) {
-	return _.flatten(_.sortBy(Object.keys(dataSubTypes), el=>el.toLowerCase()).map(function(dataSubType){
-		var datasets = dataSubTypes[dataSubType],
-			sortedOpts = sortByLabel(filterDatasets(datasets)).map(d => ({value: d.dsID, label: d.label}));
-		return [{label: dataSubType, header: true}].concat(sortedOpts);
-	}));
-=======
-
-
-function optsFromDatasets(servers) {
-	return _.flatmap(servers, (datasets, server) => {
-		let sortedOpts = sortByLabel(filterDatasets(datasets)).map(d =>
-			({value: d.dsID, label: d.label}));
-		return [{label: header(server), header: true}].concat(sortedOpts);
+			// one-time creation of a list
+			if (!_.has(dsGroups[server], group)) {
+				dsGroups[server][group] = {name: group, options: []};
+			}
+			dsGroups[server][group].options.push({value: ds.dsID, label: ds.label});
+		} else {
+			return;
+		}
 	});
->>>>>>> - Created Navigation component to manage state and behavior and presentation of buttons
+	let localGroup = dsGroups.local[localGroupKey];
+	return (localGroup ? [localGroup] : []).concat(
+		_.sortBy(dsGroups.remote, (group, name) => name.toLowerCase())
+	);
 }
 
 var DatasetSelect = React.createClass({
+	getInitialState: function() {
+		return {}
+	},
 	mixins: [deepPureRenderMixin],
 	render: function () {
-		var {children, datasets, nullOpt, ...other} = this.props,
-			options = (nullOpt ? [{value: null, label: nullOpt}] : [])
-<<<<<<< 34ca75fcf82b2d55a69e2abac0fa9cbc9e73cb4b
-				.concat(optsFromDatasets(_.groupBy(datasets, 'dataSubType')))
-=======
-				.concat(optsFromDatasets(_.groupBy(datasets, 'server')));
-
-		let label = (children && !_.isArray(this.props.children)) ? children
+		var {children, datasets, localHubUrl, value, ...other} = this.props,
+			groups = groupsFromDatasets(datasets, localHubUrl);
+		var label = (children && !_.isArray(this.props.children)) ? children
 			: <label className='datasetAnchor'>Dataset: </label>;
->>>>>>> - Created Navigation component to manage state and behavior and presentation of buttons
-
 		return (
-			<div className='form-group'>
+			<ButtonGroup stacked className='form-group'>
 				{label}
-				<Select {...other}  options={options} />
-			</div>
+				{_.map(groups, (group) => {
+					let header = {header: true, label: group.name},
+						groupIsException = _.contains(exceptions, group.name),
+						groupWithHeader = groupIsException ? [_.first(group.options)] : [header].concat(group.options),
+						choice = _.find(groupWithHeader, ds => ds.value === value) || header;
+					return (children || (!children && choice && choice.value)) ? (
+						<div key={group.name}>
+							<Select {...other} options={groupWithHeader} choice={choice}/>
+						</div>
+					) : null;
+				})}
+			</ButtonGroup>
 		);
 	}
 });
