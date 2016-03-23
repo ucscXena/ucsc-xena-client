d0d0ea172cc0b9c917bf59ec37623515c62bfdd1
Brian Craft <craft@soe.ucsc.edu>
2016-03-23 13:05:12 -0700

    Update schema.

--- docs/schema.js
+++ docs/schema.js
@@ -2,17 +2,19 @@
 /*eslint new-cap: [0], camelcase: [0] */
 'use strict';
 var S = require('schema-shorthand').schema;
-var {d, string, array, number, or, nullval, boolean, r, object} = S;
+console.log(S);
+console.log(Object.keys(S));
+var {desc, fn, string, array, arrayOf, number, or, nullval, boolean, role, object} = S;
 
-var dsID = d('dsID', 'JSON encoded host and dataset id',
+var dsID = desc('dsID', 'JSON encoded host and dataset id',
 			string(/{"host":".*","name":".*"}/));
 
-var ColumnID = d(
+var ColumnID = desc(
 	'ColumnID', 'UUID for identifying columns',
 	string(/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}/)
 );
 
-var Column = d(
+var Column = desc(
 	'Column', 'A column for display', S({
 	dsID: dsID,
 	width: number([0]),
@@ -23,7 +25,7 @@ var Column = d(
 		'geneProbeMatrix',
 		'clinicalMatrix'
 	),
-	fields: array.of(string()),
+	fields: arrayOf(string()),
 	fieldLabel: {
 		user: string(),
 		default: string()
@@ -34,17 +36,17 @@ var Column = d(
 	}}));
 
 
-var Columns = d(
+var Columns = desc(
 	'Columns', 'A set of columns', S({
 		[ColumnID]: Column
 	}));
 
-//var Cohorts = d(
-//	'Cohorts', 'A set of cohorts', array.of(string())
+//var Cohorts = desc(
+//	'Cohorts', 'A set of cohorts', arrayOf(string())
 //);
 //
-//var ColumnOrder = d(
-//	array.of('/[-0-9a-f]+/')
+//var ColumnOrder = desc(
+//	arrayOf('/[-0-9a-f]+/')
 //);
 
 var DataSubType = or(
@@ -67,7 +69,7 @@ var DataSubType = or(
 	'gene expression Array');
 
 // XXX There is also datasubtype. WTF is that?
-var Dataset = d(
+var Dataset = desc(
 	'Dataset', 'Dataset metadata',
 	S({
 		articletitle: string(),
@@ -94,12 +96,12 @@ var Dataset = d(
 	})
 );
 
-var FeatureName = d(
+var FeatureName = desc(
 	'FeatureName', 'Name of a feature. Should be unique in the dataset.',
 	string()
 );
 
-var Feature = d(
+var Feature = desc(
 	'Feature', 'Phenotype metdata',
 	S({
 		[FeatureName]: {
@@ -116,60 +118,60 @@ var Feature = d(
 	})
 );
 
-var FeaturesByDataset = d(
+var FeaturesByDataset = desc(
 	'FeaturesByDataset', 'Phenotype metadata, indexed by dataset',
 	S({
 		[dsID]: Feature
 	})
 );
 
-var FeatureID = d(
+var FeatureID = desc(
 	'FeatureID', 'Primary key for a feature',
 	S({
 		dsID: dsID,
 		name: FeatureName
 	}));
 
-var SampleID = d(
+var SampleID = desc(
 	'SampleID', 'A sample id. Must be unique within a cohort',
 	string()
 );
 
-var HeatmapData = d(
+var HeatmapData = desc(
 	'HeatmapData', 'Matrix of values for heatmap display, ordered by field and sample.',
-	array.of(r('field', array.of(r('sample', number()))))
+	arrayOf(role('field', arrayOf(role('sample', number()))))
 );
 
-var ColorSpec = d(
+var ColorSpec = desc(
 	'ColorSpec', 'A color scale variant.',
 	or(
-		array('float-pos', r('low', number()), r('high', number()), r('min', number()), r('max', number())),
-		array('float-neg', r('low', number()), r('high', number()), r('min', number()), r('max', number())),
-		array('float', r('low', number()), r('zero', number()), r('high', number()), r('min', number()), r('max', number())),
-		array('float-thresh-pos', r('zero', number()), r('high', number()), r('min', number()), r('threshold', number()), r('max', number())),
-		array('float-thresh-neg', r('low', number()), r('zero', number()), r('min', number()), r('threshold', number()), r('max', number())),
-		array('float-thresh', r('low', number()), r('zero', number()), r('high', number()), r('min', number()), r('minThreshold', number()), r('maxThreshold', number()), r('max', number())),
-		array('ordinal', r('count', number([0])))
+		array('float-pos', role('low', number()), role('high', number()), role('min', number()), role('max', number())),
+		array('float-neg', role('low', number()), role('high', number()), role('min', number()), role('max', number())),
+		array('float', role('low', number()), role('zero', number()), role('high', number()), role('min', number()), role('max', number())),
+		array('float-thresh-pos', role('zero', number()), role('high', number()), role('min', number()), role('threshold', number()), role('max', number())),
+		array('float-thresh-neg', role('low', number()), role('zero', number()), role('min', number()), role('threshold', number()), role('max', number())),
+		array('float-thresh', role('low', number()), role('zero', number()), role('high', number()), role('min', number()), role('minThreshold', number()), role('maxThreshold', number()), role('max', number())),
+		array('ordinal', role('count', number([0])))
 	)
 );
 
-var colorString = d(
+var colorString = desc(
 	'colorString', 'A css color string',
 	or(string(/#[0-9A-Fa-f]{6}/), string())
 );
 
-var Gene = d('Gene', 'A gene name', string());
-var Probe = d('Probe', 'A probe name', string());
-var Field = d('Field', 'A gene or probe name', or(Gene, Probe));
+var Gene = desc('Gene', 'A gene name', string());
+var Probe = desc('Probe', 'A probe name', string());
+var Field = desc('Field', 'A gene or probe name', or(Gene, Probe));
 
-var ProbeData = d(
+var ProbeData = desc(
 	'ProbeData', 'Data for a probe column',
 	S({
 		req: {
 			mean: {
 				[Field]: number()
 			},
-			probes: array.of(string()),
+			probes: arrayOf(string()),
 			values: {
 				[Field]: {
 					[SampleID]: number() // or null? or NaN?
@@ -177,26 +179,26 @@ var ProbeData = d(
 			}
 		},
 		features: Feature,
-		codes: array.of('FIXME'), // XXX wrong
-		bounds: array.of('FIXME'), // XXX wrong
+		codes: arrayOf('FIXME'), // XXX wrong
+		bounds: arrayOf('FIXME'), // XXX wrong
 		display: {
-			colors: array.of(ColorSpec),
+			colors: arrayOf(ColorSpec),
 			heatmap: HeatmapData
 		}
 	}));
 
 // XXX why is this not rendered as <em>string /[ACTG]*/</em>?
-var Sequence = d(
+var Sequence = desc(
 	'Sequence', 'String representation of a sequence',
 	string(/[ACTG]*/)
 );
 
-var BasePosition = d(
+var BasePosition = desc(
 	'BasePosition', 'Base position',
 	number([0])
 );
 
-var Mutation = d(
+var Mutation = desc(
 	'Mutation', 'Mutation record',
 	S({
 		alt: Sequence,
@@ -213,7 +215,7 @@ var Mutation = d(
 	})
 );
 
-var RefGene = d(
+var RefGene = desc(
 	'RefGene', 'A refGene annotation',
 	S({
 		cdsEnd: BasePosition,
@@ -228,7 +230,7 @@ var RefGene = d(
 	})
 );
 
-var MutationData = d(
+var MutationData = desc(
 	'MutationData', 'Data for a mutation column',
 	S({
 		req: object({
@@ -247,7 +249,7 @@ var MutationData = d(
 //
 // XXX oh, fudge. If we move this outside app data, then the sort is
 // also outside app data. Which is correct, but what does it break?
-var MutationViewData = d(
+var MutationViewData = desc(
 	'MutationViewData', 'Data for the mutation view',
 	S({
 		index: {},
@@ -256,7 +258,7 @@ var MutationViewData = d(
 	})
 );
 
-var VizSettings = d(
+var VizSettings = desc(
 	'VizSettings', 'User settings for visualization',
 	S({
 		max: number(),
@@ -267,12 +269,12 @@ var VizSettings = d(
 	})
 );
 
-var Application = d(
+var Application = desc(
 	'Application', 'The application state',
 	S({
 		cohort: string(),
-		cohorts: array.of(string()),
-		columnOrder: array.of(ColumnID),
+		cohorts: arrayOf(string()),
+		columnOrder: arrayOf(ColumnID),
 		columns: Columns,
 		data: {
 			[ColumnID]: or(ProbeData, MutationData)
@@ -291,11 +293,11 @@ var Application = d(
 				tte: FeatureID
 			}
 		},
-		samples: array.of(string()),
+		samples: arrayOf(string()),
 		samplesFrom: or(string(), nullval),
 		servers: {
-			default: array.of(string()),
-			user: array.of(string())
+			default: arrayOf(string()),
+			user: arrayOf(string())
 		},
 		zoom: {
 			count: number([0]),
@@ -306,18 +308,18 @@ var Application = d(
 	})
 );
 
-var KmPoint = d(
+var KmPoint = desc(
 	'KmPoint', 'A time point in a KM result. t: time; s: % surviving at t; e: event at t', S({
 		d: number(),
-		e: boolean,
-		n: number(),
+		e: role('event at time t', boolean),
+		n: role('number in study at time t', number()),
 		rate: or(nullval, number()),
-		s: number(),
-		t: number()
+		s: role('% surviving at time t', number()),
+		t: role('time', number())
 	})
 )
 
-var KmPlotData = d(
+var KmPlotData = desc(
 	'KmPlotData', 'Data for drawing KM', 
 	S({
 		ev: FeatureID,
@@ -333,11 +335,8 @@ var KmPlotData = d(
 	})
 );
 //
-//var BasePos = d('base position', number([0]));
-var Chrom = d('Chrom', 'chrom', string(/chr[0-9]+/));
-
-// Where does 'description' live? On parameter, or on schema? If both, does d() overwrite
-// the one on the schema?
+//var BasePos = desc('base position', number([0]));
+var Chrom = desc('Chrom', 'chrom', string(/chr[0-9]+/));
 
 // This describes user intent. Currently, we have only 'fields', which can be probes or genes.
 // clinicalMatrix [field]
@@ -363,13 +362,13 @@ var Chrom = d('Chrom', 'chrom', string(/chr[0-9]+/));
 // The first thing I will want to do is register widgets based on 1 vs many. So, seems useful to use a data tag. An
 // alternative is to dispatch on fields lengths.
 //
-//var GenomicPosition = d(
+//var GenomicPosition = desc(
 //	'GenomicPosition', 'genomic position (intent)',
 //	or(
-//		array('probes', array.of(Probe)),
-//		array('gene', array.of(Gene)),
+//		array('probes', arrayOf(Probe)),
+//		array('gene', arrayOf(Gene)),
 //		array('exons', Gene),
-//		array('chrom', Chrom, d('start', BasePos), d('end', BasePos))
+//		array('chrom', Chrom, desc('start', BasePos), desc('end', BasePos))
 //	)
 //);
 
@@ -392,36 +391,39 @@ var Chrom = d('Chrom', 'chrom', string(/chr[0-9]+/));
 
 // ChromPosition is internally converted to an array of (start, end)
 
-//var ChromIntervals = d(
+//var ChromIntervals = desc(
 //	'chrom intervals', {
 //		chrom: string(),
-//		intervals: array.of(
+//		intervals: arrayOf(
 //			array(BasePos, BasePos)
 //		)
 //	}
 //);
 
 //
-//var PixelPos = d('pixel offset', number([0]));
+//var PixelPos = desc('pixel offset', number([0]));
 
-//var Partition = array.of(array(PixelPos, PixelPos));
+//var Partition = arrayOf(array(PixelPos, PixelPos));
 
-// XXX note that r() and d() defeat the reference equality check when inlining schema. Ouch.
-var ChromLayout = d('ChromLayout', 'A chromosome layout. Currently limited to a single chrom.',
-					S([[r('start', BasePosition), r('end', BasePosition)]])
+// XXX note that role() and desc() defeat the reference equality check when inlining schema. Ouch.
+var ChromLayout = desc('ChromLayout', 'A chromosome layout. Currently limited to a single chrom.',
+					S([[role('start', BasePosition), role('end', BasePosition)]])
 				   );
 
-var ScreenLayout = d('ScreenLayout', 'A screen layout',
-					S([[r('start px', number([0])), r('end px', number([0]))]])
+var ScreenLayout = desc('ScreenLayout', 'A screen layout',
+					S([[role('start px', number([0])), role('end px', number([0]))]])
 					);
 
-var Layout = d('Layout', 'Screen and chrom layout',
+var Layout = desc('Layout', 'Screen and chrom layout',
 			  S({chrom: ChromLayout, screen: ScreenLayout, reversed: boolean}));
 
-var KmPlotProps = d('KmPlotProps', 'KmPlot Component properties',
+var Action = desc('Action', 'Action on state',
+				 [role('type', string())]);
+
+var KmPlotProps = desc('KmPlotProps', 'KmPlot Component properties',
 	S({
-		callback: "Function for emitting events",
-		eventClose: d('event name', 'Action type to issue when "close" is requested by user', string()),
+		callback: role('State updater', fn(Action)),
+		eventClose: role('Action type to issue on "close"', string()),
 		features: FeaturesByDataset,
 		km: KmPlotData
 	})
@@ -458,5 +460,6 @@ module.exports = [
 	colorString,
 	KmPoint,
 	KmPlotData,
-	KmPlotProps
+	KmPlotProps,
+	Action
 ];
