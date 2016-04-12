daacde7259d5022c0c3fcf09faff42e5fc67b50a
Brian Craft <craft@soe.ucsc.edu>
2016-04-12 12:57:31 -0700

    Support currying multimethods.

--- js/multi.js
+++ js/multi.js
@@ -1,6 +1,6 @@
 /*eslint strict: [2, "function"] */
 /*global define: false */
-define([], function () {
+define(['./underscore_ext'], function (_) {
 	'use strict';
 	// Ad hoc polymorphism, a bit like clojure's multimethods: a way to dispatch
 	// based on any criteria (instead of just on 'this').
@@ -11,25 +11,29 @@ define([], function () {
 	// scribble.add('crayon', function(name, val) { crayon('name'); })
 	//
 	// Set fn.dflt if a default is required.
-	// The dispatch function may be modified by setting fn.dispatchfn.
-	function multi(dispatchfn) {
+	// The dispatch function may be modified by calling fn.dispatchfn.
+	// If arity is specified, the function wlil be curried.
+	function multi(dispatchfn, arity) {
 		var methods = {},
+			getmethod = function() {
+				var dispatch = dispatchfn.apply(this, arguments),
+					method = methods[dispatch] || fn.dflt;
+
+				if (typeof method !== 'function') {
+					throw new Error('No method for ' + dispatch);
+				}
+				return method;
+			},
 			fn = function () {
-				var method = fn.getmethod.apply(this, arguments);
+				var method = getmethod.apply(this, arguments);
 
 				return method.apply(this, arguments);
 			};
 
-		fn.getmethod = function() {
-			var dispatch = fn.dispatchfn.apply(this, arguments),
-				method = methods[dispatch] || fn.dflt;
-
-			if (typeof method !== 'function') {
-				throw new Error('No method for ' + dispatch);
-			}
-			return method;
-		};
-		fn.dispatchfn = dispatchfn;
+		if (arguments.length > 1) {
+			fn = _.curryN(arity, fn);
+		}
+		fn.dispatchfn = fn => dispatchfn = fn;
 		fn.add = function (dispatch, fn) {
 			methods[dispatch] = fn;
 		};
