8722088c091fd1f76a247cb73cb1106e1f2f7141
Brian Craft <craft@soe.ucsc.edu>
2016-04-18 15:27:55 -0700

    collectAlignedResults.

--- js/controllers/errors.js
+++ js/controllers/errors.js
@@ -14,18 +14,19 @@ function reifyErrors(obs, context) {
 
 // From an array of values, some of them errors, combine the successes into a
 // single event, combine the errors and re-throw. This is a flatmap function.
-function collectResults(arr, selector) {
+var collectResults = _.curry((filter, arr, selector) => {
 	var groups = _.groupBy(arr, r => r instanceof Error),
-		response = selector(groups.false || []),
+		response = selector(filter ? (groups.false || []) : arr),
 		errors = groups.true;
 
 	return errors ?
 		Rx.Observable.return(response)
 			.concat(Rx.Observable.throw(compositeError('Composite Error', ...errors))) :
 		Rx.Observable.return(response);
-}
+});
 
 module.exports = {
 	reifyErrors,
-	collectResults
+	collectResults: collectResults(true),
+	collectAlignedResults: collectResults(false)
 };
