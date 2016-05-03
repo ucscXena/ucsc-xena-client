0b60a17e1ae25cba035f1938b1d65a77daaf9d25
Brian Craft <bcboy@thecraftstudio.com>
2016-05-03 16:02:57 -0700

    Add link to beta site.

--- js/nav.js
+++ js/nav.js
@@ -22,8 +22,11 @@ var links = [
 	{href: "../hub/", label: "Data Hubs"},
 	{href: "https://galaxyxena.soe.ucsc.edu", label: "Galaxy Xena"},
 	{href: "https://genome-cancer.ucsc.edu/download/public/get-xena/index.html", label: "Local Xena"},
+	{href: "../../composite/heatmap/", label: "Beta Features", target: '_blank'},
 	{href: "https://docs.google.com/a/soe.ucsc.edu/document/d/1CIWj6L8LAaHFmLek3yrbrjFKRm_l3Sy73lJ4wY-WM8Y", label: "Help"}
 ];
+
+var target = l => l.target ? {target: l.target} : {};
 
 var XenaNav = React.createClass({
 	getInitialState: function() {
@@ -38,7 +41,7 @@ var XenaNav = React.createClass({
 		return (
 			<Navbar>
 				<Nav activeKey={activeTab} bsStyle="pills">
-					{_.map(links, l => <NavItem key={l.href} eventKey={l.label} href={l.href}>{l.label}</NavItem>)}
+					{_.map(links, l => <NavItem {...target(l)} key={l.href} eventKey={l.label} href={l.href}>{l.label}</NavItem>)}
 				</Nav>
 			</Navbar>
 		);
