9db06b428f7f28e30a2dde84c50b9bd7dac67c95
Brian Craft <craft@soe.ucsc.edu>
2016-05-25 14:39:41 -0700

    Move Legend to js/views.

--- js/views/Legend.js
+++ js/views/Legend.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var React = require('react');
-var _ = require('./underscore_ext');
+var _ = require('../underscore_ext');
 var Row = require('react-bootstrap/lib/Row');
 var Col = require('react-bootstrap/lib/Col');
 require('./Legend.css');
