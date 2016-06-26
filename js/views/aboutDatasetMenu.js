32d03e8f84282d975f868e8d177997ef27f71285
Brian Craft <bcboy@thecraftstudio.com>
2016-06-26 09:43:45 -0700

    Parameterize root url for 'about dataset'

--- js/views/aboutDatasetMenu.js
+++ js/views/aboutDatasetMenu.js
@@ -5,20 +5,20 @@ var MenuItem = require('react-bootstrap/lib/MenuItem');
 var _ = require('../underscore_ext');
 var xenaQuery = require('../xenaQuery'); // XXX does this belong here?
 
-function onAbout(dsID) {
+function onAbout(dsID, root) {
 	var [host, dataset] = xenaQuery.parse_host(dsID);
-	var url = `../datapages/?dataset=${encodeURIComponent(dataset)}&host=${encodeURIComponent(host)}`;
+	var url = `${root}/datapages/?dataset=${encodeURIComponent(dataset)}&host=${encodeURIComponent(host)}`;
 	window.open(url);
 }
 
-var getAbout = (dsID, text) => (
-	<MenuItem key={dsID} onSelect={() => onAbout(dsID)}>{text} </MenuItem>);
+var getAbout = (dsID, root, text) => (
+	<MenuItem key={dsID} onSelect={() => onAbout(dsID, root)}>{text} </MenuItem>);
 
-function aboutDatasetMenu({dsIDs, label}) {
+function aboutDatasetMenu({dsIDs, label}, root = '..') {
 	if (dsIDs.length === 0) {
 		return null;
 	} else if (dsIDs.length === 1) {
-		return getAbout(dsIDs[0], 'About the Dataset');
+		return getAbout(dsIDs[0], root, 'About the Dataset');
 	}
 	return [
 		<MenuItem key='d0' divider/>,
