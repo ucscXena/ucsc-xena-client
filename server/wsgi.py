5e78f05b1ef0e58b4ca1df9e2962a067831fe1d3
Brian Craft <bcboy@thecraftstudio.com>
2016-09-01 22:09:47 -0700

    Fix django wsgi paths.

--- server/wsgi.py
+++ server/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+appdir = os.path.join(os.path.dirname(__file__), '..')
 
+site.addsitedir(os.path.join(appdir, '../virtualenv/lib/python2.7/site-packages'))
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(appdir)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
