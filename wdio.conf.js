480b0d7d7b8b5f9c041835cb10d765c3a2de4bcd
Brian Craft <craft@soe.ucsc.edu>
2016-10-11 10:23:36 -0700

    Rename chimp->selenium.

--- wdio.conf.js
+++ wdio.conf.js
@@ -11,7 +11,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/chimp/*.js'
+        './test/selenium/*.js'
     ],
     // Patterns to exclude.
     exclude: [
