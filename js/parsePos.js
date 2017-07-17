434bd1d496207396a138771920b3ec7f4dd242cd
jingchunzhu <jingchunzhu@gmail.com>
2017-07-17 02:31:42 -0700

    Revert "reverse parsePos"
    This reverts commit 4a3f5e7717f109e756cb6cf8c63d8adf27ea5b4d.

--- js/parsePos.js
+++ js/parsePos.js
@@ -12,8 +12,7 @@ var M = 1000 * 1000;
 module.exports = function (text, assembly) {
 	// strip spaces, cvt to lower, match chr1:2-chr3:4 format
 	text = unicode.normalize(text).replace(/ /g, '').toLowerCase();
-	var pos = text.match(/^(chr[0-9xyXY]+)([pq]?)(:([0-9,]+)-([0-9,]+))?$/);
-	//var pos = text.match(/^(chr[0-9xyXY]+)([pq]?)(:([0-9,]+)(-([0-9,]+))?)?$/);
+	var pos = text.match(/^(chr[0-9xyXY]+)([pq]?)(:([0-9,]+)(-([0-9,]+))?)?$/);
 	if (pos) {
 		let chrom = pos[1].replace(/x/, 'X').replace(/y/, 'Y'),
 			cm = _.getIn(centromere, [assembly, chrom]),
@@ -21,12 +20,11 @@ module.exports = function (text, assembly) {
 			baseStart, baseEnd;
 
 		if (pos[3] !== undefined) {
-			baseEnd = clip(maxEnd, toInt(pos[5].replace(/,/g, '')));
 			baseStart = clip(baseEnd, toInt(pos[4].replace(/,/g, '')));
-			/*baseEnd = baseStart;
+			baseEnd = baseStart;
 			if (pos[6] !== undefined) {
 				baseEnd = clip(maxEnd, toInt(pos[6].replace(/,/g, '')));
-			}*/
+			}
 		} else if (cm && pos[2] === 'p') {
 			baseStart = 1;
 			baseEnd = cm - 1;
