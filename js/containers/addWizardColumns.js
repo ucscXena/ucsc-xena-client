68f1167fb877dd4ca522d23b1339d2a272a160e9
Brian Craft <bcboy@thecraftstudio.com>
2017-08-02 16:01:31 -0700

    Fix column label.

--- js/containers/addWizardColumns.js
+++ js/containers/addWizardColumns.js
@@ -63,7 +63,7 @@ function columnSettings(datasets, features, dsID, input, probes) {
 		dsID,
 		defaultNormalization: meta.colnormalization,
 		// XXX this assumes fields[0] doesn't appear in features if ds is genomic
-		fieldLabel: _.get(features, [dsID, fields[0], 'longtitle'], fields.join(', ')),
+		fieldLabel: _.getIn(features, [dsID, fields[0], 'longtitle'], fields.join(', ')),
 		colorClass: defaultColorClass,
 		assembly: meta.assembly
 	};
