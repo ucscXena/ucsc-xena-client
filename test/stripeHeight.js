8eaa132e310f7a6421f935bef78df953ab5aff74
Brian Craft <bcboy@thecraftstudio.com>
2017-09-14 14:23:09 -0700

    update stripeHeight tests.

--- test/stripeHeight.js
+++ test/stripeHeight.js
@@ -5,17 +5,17 @@ var {stripeHeight} = require('../js/drawSamples');
 var assert = require('assert');
 
 describe('stripeHeight', () => {
-	it('should return 1 for (100, 200, 1000)', () => {
-		assert.equal(1, stripeHeight(100, 200, 1000));
+	it('should return 5 for (100, 200, 1000)', () => {
+		assert.equal(5, stripeHeight(100, 200, 1000));
 	});
 	it('should return 5 for (100, 200, 500)', () => {
 		assert.equal(5, stripeHeight(100, 200, 500));
 	});
-	it('should return 10 for (100, 200, 100)', () => {
-		assert.equal(10, stripeHeight(100, 200, 100));
+	it('should return 50 for (100, 200, 100)', () => {
+		assert.equal(50, stripeHeight(100, 200, 100));
 	});
-	it('should return 100 for (100, 200, 10)', () => {
-		assert.equal(100, stripeHeight(100, 200, 10));
+	it('should return 500 for (100, 200, 10)', () => {
+		assert.equal(500, stripeHeight(100, 200, 10));
 	});
 });
 
