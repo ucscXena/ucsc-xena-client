090e78251c063d6a394ac8e6da5e6e329f7f6473
Brian Craft <craft@soe.ucsc.edu>
2018-02-12 15:45:49 -0800

    ties /query/ param term -> text

--- ties/forms.py
+++ ties/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 
 class QuerySearchForm(forms.Form):
-    term = forms.CharField(required=False)
+    text = forms.CharField(required=False)
     concept = forms.CharField(required=False)
     limit = forms.IntegerField(required=False)
     neg = forms.BooleanField(required=False)
     def clean(self):
         cleaned_data = super(QuerySearchForm, self).clean()
-        term = cleaned_data.get('term')
+        text = cleaned_data.get('text')
         concept = cleaned_data.get('concept')
-        if term and concept:
-            raise forms.ValidationError("Passed both term and concept. Only one allowed.")
-        if not term and not concept:
-            raise forms.ValidationError("Either term or concept is required")
+        if text and concept:
+            raise forms.ValidationError("Passed both text and concept. Only one allowed.")
+        if not text and not concept:
+            raise forms.ValidationError("Either text or concept is required")
 
 class TermForm(forms.Form):
     term = forms.CharField(required=False)
