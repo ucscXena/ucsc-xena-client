aa00fa95514d0b2c45e8c3cbede63bf4877d11fa
Brian Craft <bcboy@thecraftstudio.com>
2018-04-08 20:34:46 -0700

    Update TIES relay methods

--- ties/urls.py
+++ ties/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from ties.views import query, search, documents
+from ties.views import query, search, documents, doc_list
 
 urlpatterns = [
-        url(r'^query/$', query),
-        url(r'^search/$', search),
-        url(r'^documents/([0-9]+)$', documents),
+    url(r'^query/$', query),
+    url(r'^search/$', search),
+    url(r'^documents/list$', doc_list),
+    url(r'^documents/([0-9]+)$', documents),
 ]
