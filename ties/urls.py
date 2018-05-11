b818d03e7124452108d25d0686ad1abdb84eb05d
Brian Craft <bcboy@thecraftstudio.com>
2018-05-10 17:04:40 -0700

    Add ties documents/filter method.

--- ties/urls.py
+++ ties/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from ties.views import query, search, documents, doc_list
+from ties.views import query, search, documents, doc_list, doc_filter
 
 urlpatterns = [
     url(r'^query/$', query),
     url(r'^search/$', search),
     url(r'^documents/list$', doc_list),
+    url(r'^documents/filter$', doc_filter),
     url(r'^documents/([0-9]+)$', documents),
 ]
