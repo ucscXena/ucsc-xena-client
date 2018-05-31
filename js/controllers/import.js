98c54b016c32b5aea96a374e34b4ec7b3a75da2a
Kristupas Repecka <kristupas.repecka@cern.ch>
2018-05-31 22:27:05 +0300

    form creation. file format, data type inputs added

--- js/controllers/import.js
+++ js/controllers/import.js
@@ -2,25 +2,27 @@
 'use strict';
 import Rx from '../rx';
 import { make, mount, compose } from './utils';
+import { encodeObject } from '../util';
+import { isArray, merge } from "../underscore_ext";
 import { servers } from '../defaultServers';
 
 
 const postFile = (file) => {
     const payload = {
-        //headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         url: `${servers.localHub}/upload/`,
         body: file,
+        responseType: 'text',
         method: 'POST',
         crossDomain: true
     }
 
-    return Rx.Observable.ajax(payload).map(r => console.log(r));
+    return Rx.Observable.ajax(payload).map(r => r.status);
 }
 
 var importControls = {
-    'import-file-post!': (serverBus, state, newState, file) => serverBus.next(['import-file', postFile(file)]),
-    'import-file': (a, b, c) => {
-        console.log(a);
+    'import-file-post!': (serverBus, state, newState, file) => serverBus.next(['import-file-done', postFile(file)]),
+    'import-file-done': (a, b, c) => {
+        console.log(b);
     }
 }
 
--- js/import/ImportPage.js
+++ js/import/ImportPage.js
@@ -5,9 +5,46 @@ import styles from './ImportPage.module.css';
 import appTheme from '../appTheme';
 
 import { ThemeProvider } from 'react-css-themr';
-import { Input, Button } from 'react-toolbox/lib';
+import {
+	Input, Button, Dropdown, Checkbox
 
-// import c
+} from 'react-toolbox/lib';
+
+import DefaultTextInput from '../views/DefaultTextInput';
+
+const formatOptions = [
+	{
+		label: 'ROWs (identifiers)  x  COLUMNs (samples) -- often genomic data matrix',
+		value: 'genomicMatrix'
+	},
+	{
+		label: 'ROWs (samples)  x  COLUMNs (identifiers) -- often phenotype data',
+		value: 'clinicalMatrix'
+	},
+	{
+		label: 'Mutation by Position',
+		value: 'mutationVector'
+	}
+];
+
+const dataTypes = [
+	"filter",
+	"phenotype",
+	"copy number",
+	"DNA methylation",
+	"exon expression",
+	"gene expression",
+	"gene expression RNAseq",
+	"gene expression Array",
+	"miRNA expression",
+	"somatic mutation (SNP and small INDELs)",
+	"somatic mutation (structural variant)",
+	"somatic mutation (gene-level)",
+	"protein expression RPPA",
+	"PARADIGM pathway activity"
+];
+
+const dataTypeOptions = dataTypes.map(type => ({ label: type, value: type }));
 
 
 const readFile = (file) => {
@@ -18,7 +55,14 @@ class ImportPage extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			file: null
+			// move to store
+			file: null,
+			fileFormat: '',
+			dataType: '',
+			customDataType: '', //is it really needed
+
+			//ui state
+			hasOwnDataType: false
 		};
 	}
 
@@ -26,6 +70,38 @@ class ImportPage extends React.Component {
 		return (
 			<div className={styles.container}>
 				<Input type='file' name='importFile' onChange={this.handleFileSelected} />
+
+				<Dropdown onChange={this.handleFileFormatChange}
+					source={formatOptions}
+					value={this.state.value}
+					allowBlank={false}
+					label="File format"
+					className={styles.field}
+				/>
+
+				{/* Extract */}
+				<div>
+					{!this.state.hasOwnDataType &&
+						<Dropdown onChange={this.handleDataTypeChange}
+							source={dataTypeOptions}
+							value={this.state.dataType}
+							label="Type of data"
+							className={[styles.field, styles.typeBox].join(' ')}
+						/>
+					}
+					{this.state.hasOwnDataType &&
+						<Input type='text' name='dataType' label="Enter data type"
+							onChange={this.handleCustomDataTypeChange}
+							value={this.state.customDataType}
+							className={[styles.field, styles.typeBox].join(' ')} />
+					}
+
+					<Checkbox onChange={this.handleHasOwnDataTypeChange}
+						checked={this.state.hasOwnDataType}
+						label="Or enter your own type"
+						className={styles.typeBox}
+					/>
+				</div>
 
 				<Button icon='save' label='Save' raised onClick={this.handleSubmitClicked} />
 			</div>
@@ -38,11 +114,21 @@ class ImportPage extends React.Component {
 		}
 	}
 
+	handleFileFormatChange = newFormat => this.setState({ fileFormat: newFormat });
+
+	handleHasOwnDataTypeChange = newValue => this.setState({ hasOwnDataType: newValue });
+
+	handleCustomDataTypeChange = newValue => this.setState({ customDataType: newValue });
+
+	handleDataTypeChange = newType => this.setState({ dataType: newType });
+
 	handleSubmitClicked = () => {
 		const reader = new FileReader();
 
 		reader.onload = (e) => {
-			this.props.callback(['import-file', { filename: this.state.file.name, bytes: e.target.result }]);
+			var formData = new FormData();
+			formData.append("file", new Blob(new Uint16Array(e.target.result)), this.state.file.name);
+			this.props.callback(['import-file', formData]);
 		};
 		reader.readAsArrayBuffer(this.state.file);
 	}
