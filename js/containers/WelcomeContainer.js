4e93df3ff8f7622ea7666a405457cc22febcb512
jingchunzhu <jingchunzhu@gmail.com>
2018-08-28 13:11:22 -0700

    Welcome banner url updates

--- js/containers/WelcomeContainer.js
+++ js/containers/WelcomeContainer.js
@@ -7,25 +7,25 @@ var Welcome = require('../views/Welcome');
 var {rxEvents} = require('../react-utils');
 
 var links = [
-	['heatmap', 'ef22bef7c049426ceb011f0a7914ddc4',
+	['heatmap', 'bc7f3f46b042bcf5c099439c2816ff01',
 		'FOXM1a, FOXM1b, FOXM1c transcript expression in tumor vs. normal samples'],
-	['heatmap', 'a659e770d2599acfad6a8f9b5ffbe57f',
-		'Mutation pile-ups in intron enhancers in ICGC lymphoma'],
-	['heatmap', '6946d911d6b0cd473c442fe99f6a695a',
-		'KM plot (overall survival) of breast cancer PAM50 subtypes'],
-	['heatmap', 'e5329f4cc13b80c91d3fd6810aac1a93',
-		'Copy number for EGFR, PTEN, chromosome 1, 7, 10, 19 in TCGA brain tumors'],
-	['heatmap', 'f9d1b795c2cb324d227a3a0472c3427c',
-		'PDL1 and PD1 expression across 39 cancer types in TCGA and TARGET'],
-	['heatmap', '9875171572aa5f9df2d5e1fc937fbe8e',
+	['heatmap', 'cd6d8adead7d720fea7df197dc807147',
 		'ERG-TMPRSS2 fusion by chr21 deletion in prostate cancer'],
-	['heatmap', '54a1a2d7ebf3a5e091d58431c23df85a',
-		'Two pathways to telomere lengthening in lower grade glioma: TERT & ATRX'],
-	['heatmap', '86aa2f71f08db3fe81f192915a303d97',
+	['heatmap', 'e617e2f31b2a02690e80f4b6acc55ff3',
+		'KM plot (overall survival) of breast cancer PAM50 subtypes'],
+	['heatmap', '937469da6dfa9171cd255884b53cb75e',
 		'Genetic separation of lower grade gliomas: one characterized by loss of chromosomes 1p & 19q, \
 		the other by TP53 & ATRX mutations.'],
+	['heatmap', '3f4b3fa8901f4c24f5f791e036566424',
+		'PDL1 and PD1 expression across 39 cancer types in TCGA and TARGET'],
+	['heatmap', '05690ede046dc7b9f98ea78d1cbc5c2e',
+		'MGMT promoter DNA methylation is inversely correlated with MGMT expression'],
+	['heatmap', '8772bbc8f47c5cda31e4737c7f88444d',
+		'Copy number for EGFR, PTEN, chromosome 1, 7, 10, 19 in TCGA brain tumors'],
 	['transcripts', '25a9782db8c1166ce7bae6686e98124c',
-		'KRAS isoform expression in TCGA pancreatic cancer vs. GTEx pancreas normal']];
+		'KRAS isoform expression in TCGA pancreatic cancer vs. GTEx pancreas normal'],
+	['heatmap', 'b62b660e6029947f51ce5745945cce90',
+		'Mutation pile-ups in intron enhancers in ICGC lymphoma']];
 
 
 var evToIndex = ev => parseInt(ev.currentTarget.dataset.index, 10);
