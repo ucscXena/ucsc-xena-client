520327a8dd76ed8886f223dfe6603eaf4b65b948
Brian Craft <bcboy@thecraftstudio.com>
2018-08-30 20:14:50 -0700

    Handle transcript bookmarks w/treehouse filter

--- js/migrateState.js
+++ js/migrateState.js
@@ -47,8 +47,9 @@ var migrations = [
 
 // treehouse is down & mucking up bookmarks
 var omitTreehouse = state =>
-	updateIn(state, ['spreadsheet', 'servers'],
-			servers => omit(servers, 'https://xena.treehouse.gi.ucsc.edu', 'https://xena.treehouse.gi.ucsc.edu:443'));
+	state.spreadsheet ? updateIn(state, ['spreadsheet', 'servers'],
+			servers => omit(servers, 'https://xena.treehouse.gi.ucsc.edu', 'https://xena.treehouse.gi.ucsc.edu:443')) :
+		state;
 
 function apply(state) {
 	var v = getVersion(state),
