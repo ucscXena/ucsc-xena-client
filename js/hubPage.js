0721684ec230f0116f75e60a48b24ab9bfdbe3e9
Brian Craft <craft@soe.ucsc.edu>
2018-09-20 16:01:43 -0700

    Add LaunchHelper to data hubs page.

--- js/hubPage.js
+++ js/hubPage.js
@@ -15,11 +15,12 @@ var typStyles = require('../css/typography.module.css');
 
 var {testHost} = require('./xenaQuery');
 var _ = require('./underscore_ext');
-var {serverNames} = require('./defaultServers');
+var {servers: {localHub}, serverNames} = require('./defaultServers');
 var styles = require('./hubPage.module.css');
 var {parseServer, getHubParams} = require('./hubParams');
 var nav = require('./nav');
 var {encodeObject} = require('./util');
+import wrapLaunchHelper from './LaunchHelper';
 
 var RETURN = 13;
 
@@ -37,120 +38,131 @@ var reqStatus = (ping) =>
 
 var checkHost = host => testHost(host).take(1).map(v => ({[host]: v}));
 
-class Hub extends React.Component {
-	state = {
-	    ping: {}
-	};
+var Hub = wrapLaunchHelper(
+	props => _.getIn(props, ['state', 'spreadsheet', 'servers', localHub, 'user']),
+	class extends React.Component {
+		static displayName = 'Hub';
 
-	onNavigate = (page, params) => {
-		this.props.callback(['navigate', page, params]);
-	};
+		state = {
+			ping: {}
+		};
 
-	componentDidMount() {
-		// XXX Use a connector to get rid of selector, here.
-		// Or use a sub-component.
-		var {state, selector} = this.props,
-			allHosts = _.keys(selector(state));
+		onNavigate = (page, params) => {
+			this.props.callback(['navigate', page, params]);
+		};
 
-		nav({activeLink: 'hub', onNavigate: this.onNavigate});
+		componentDidMount() {
+			// XXX Use a connector to get rid of selector, here.
+			// Or use a sub-component.
+			var {state, selector} = this.props,
+				allHosts = _.keys(selector(state));
 
-		this.sub = Rx.Observable.from(allHosts.map(checkHost))
-			.mergeAll()
-			.subscribe(this.updatePing);
-	}
+			nav({activeLink: 'hub', onNavigate: this.onNavigate});
+			this.ping = new Rx.Subject();
 
-	componentWillUnmount() {
-		this.sub.unsubscribe();
-	}
+			this.sub = this.ping.switchMapTo(Rx.Observable.from(allHosts.map(checkHost)).mergeAll())
+				.subscribe(this.updatePing);
 
-	componentWillReceiveProps(newProps) {
-		var {ping} = this.state,
-			{state, selector} = newProps,
-			servers = selector(state),
-			old = _.omit(ping, _.keys(servers));
-
-		this.setState({ping: _.omit(ping, old)});
-
-		_.difference(_.keys(servers), _.keys(ping))
-			.forEach(h => checkHost(h).subscribe(this.updatePing));
-	}
-
-	updatePing = (h) => {
-		this.setState({ping: {...this.state.ping, ...h}});
-	};
-
-	onKeyDown = (ev) => {
-		if (ev.keyCode === RETURN) {
-			ev.preventDefault();
-			this.onAdd();
+			this.ping.next();
 		}
-	};
 
-	onSelect = (isOn, ev) => {
-		var {checked} = ev.target,
-			host = ev.target.getAttribute('data-host');
-		this.props.callback([checked ? 'enable-host' : 'disable-host', host, 'user']);
-	};
-
-	onAdd = () => {
-		var target = this.refs.newHost,
-			value = target.value.trim();
-		if (value !== '') {
-			this.props.callback(['add-host', parseServer(value)]);
-			target.value = '';
+		onStartup = () => {
+			this.ping.next();
 		}
-	};
 
-	onRemove = (ev) => {
-		var host = ev.currentTarget.getAttribute('data-host');
-		this.props.callback(['remove-host', host]);
-	};
+		componentWillUnmount() {
+			this.sub.unsubscribe();
+		}
 
-	render() {
-		var {state, selector} = this.props,
-			hubParams = getHubParams(state),
-			{ping} = this.state,
-			servers = selector(state),
-			hostList = _.mapObject(servers, (s, h) => ({
-				selected: s.user,
-				host: h,
-				name: getName(h),
-				statusStr: getStatus(s.user, ping[h]),
-				reqStatus: reqStatus(ping[h])
-			}));
-		return (
-			<div className={styles.hubPage}>
-				<h1 className={typStyles.mdHeadline}>Data Hubs</h1>
-				<Card>
-					<ul className={styles.hubList}>
-						{_.values(hostList).map(h => (
-						<li key={h.host}>
-							<Checkbox className={styles.checkbox} onChange={this.onSelect} checked={h.selected}
-									  data-host={h.host}/>
-							<div className={styles.statusContainer}>
-								<span className={classNames(styles.status, getStyle(h.statusStr))}>{h.statusStr}</span>
-							</div>
-							<div className={styles.hubNameContainer}>
-								<a href={`../datapages/?${encodeObject({host: h.host, ...hubParams})}`}>
-									{h.name}{h.reqStatus}
-								</a>
-							</div>
-							<i className={classNames('material-icons', styles.remove)} data-host={h.host}
-							   onClick={this.onRemove}>close</i>
-						</li>
-						))}
-						<li>
-							<div className={styles.hostForm}>
-								<input className={styles.input} onKeyDown={this.onKeyDown} ref='newHost'
-									   type='text' placeholder='Add Hub'/>
-								<Button onClick={this.onAdd} accent>Add</Button>
-							</div>
-						</li>
-					</ul>
-				</Card>
-			</div>);
-	}
-}
+		componentWillReceiveProps(newProps) {
+			var {ping} = this.state,
+				{state, selector} = newProps,
+				servers = selector(state),
+				old = _.omit(ping, _.keys(servers));
+
+			this.setState({ping: _.omit(ping, old)});
+
+			_.difference(_.keys(servers), _.keys(ping))
+				.forEach(h => checkHost(h).subscribe(this.updatePing));
+		}
+
+		updatePing = (h) => {
+			this.setState({ping: {...this.state.ping, ...h}});
+		};
+
+		onKeyDown = (ev) => {
+			if (ev.keyCode === RETURN) {
+				ev.preventDefault();
+				this.onAdd();
+			}
+		};
+
+		onSelect = (isOn, ev) => {
+			var {checked} = ev.target,
+				host = ev.target.getAttribute('data-host');
+			this.props.callback([checked ? 'enable-host' : 'disable-host', host, 'user']);
+		};
+
+		onAdd = () => {
+			var target = this.refs.newHost,
+				value = target.value.trim();
+			if (value !== '') {
+				this.props.callback(['add-host', parseServer(value)]);
+				target.value = '';
+			}
+		};
+
+		onRemove = (ev) => {
+			var host = ev.currentTarget.getAttribute('data-host');
+			this.props.callback(['remove-host', host]);
+		};
+
+		render() {
+			var {state, selector, children, badge} = this.props,
+				hubParams = getHubParams(state),
+				{ping} = this.state,
+				servers = selector(state),
+				hostList = _.mapObject(servers, (s, h) => ({
+					selected: s.user,
+					host: h,
+					name: getName(h),
+					statusStr: getStatus(s.user, ping[h]),
+					reqStatus: reqStatus(ping[h])
+				}));
+			return (
+				<div className={styles.hubPage}>
+					{children /* LaunchHelper */}
+					<h1 className={typStyles.mdHeadline}>Data Hubs</h1>
+					<Card>
+						<ul className={styles.hubList}>
+							{_.values(hostList).map(h => (
+							<li key={h.host}>
+								<Checkbox className={styles.checkbox} onChange={this.onSelect} checked={h.selected}
+										  data-host={h.host}/>
+								<div className={styles.statusContainer}>
+									<span className={classNames(styles.status, getStyle(h.statusStr))}>{h.statusStr}</span>
+								</div>
+								<div className={styles.hubNameContainer}>
+									<a href={`../datapages/?${encodeObject({host: h.host, ...hubParams})}`}>
+										{h.name}{h.reqStatus}{h.host === localHub ? badge : null}
+									</a>
+								</div>
+								<i className={classNames('material-icons', styles.remove)} data-host={h.host}
+								   onClick={this.onRemove}>close</i>
+							</li>
+							))}
+							<li>
+								<div className={styles.hostForm}>
+									<input className={styles.input} onKeyDown={this.onKeyDown} ref='newHost'
+										   type='text' placeholder='Add Hub'/>
+									<Button onClick={this.onAdd} accent>Add</Button>
+								</div>
+							</li>
+						</ul>
+					</Card>
+				</div>);
+		}
+	});
 
 var selector = state => state.spreadsheet.servers;
 
