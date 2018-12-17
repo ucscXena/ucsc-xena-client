b3977284a05d7f82f9218760ababe1945cc76b13
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-12-17 13:14:16 +1000

    Chrom zoom controls

--- js/views/ColCard.js
+++ js/views/ColCard.js
@@ -40,7 +40,7 @@ class ColCard extends React.Component {
 				</div>
 				<div className={compStyles.titleContainer}>
 					<CardTitle className={compStyles.title} title={title} subtitle={subtitle}/>
-					{xAnnotationZoom ? <div className={compStyles.geneZoomReference} onClick={onClick}>Zoomed<i className='material-icons'>cancel</i></div> : null }
+					{xAnnotationZoom ? <div className={compStyles.geneZoomControl} onClick={onClick}>Zoomed<i className='material-icons'>cancel</i></div> : null }
 				</div>
 				{children}
 			</Card>
