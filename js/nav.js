dfbed7c2301241361346bb342f8b59e15cb043a2
Nathan Dunn <ndunn@me.com>
2019-02-18 12:03:15 -0800

    added a nav link for geneSets

--- js/nav.js
+++ js/nav.js
@@ -46,6 +46,13 @@ var helpLink = {
 	target: '_blank'
 };
 
+var geneSetsLink = {
+    // href: 'https://xenageneset.berkeleybop.io/xena/',
+    href: 'http://xenademo.berkeleybop.io/xena/',
+    label: 'Gene Sets',
+    target: '_blank'
+};
+
 var active = (l, activeLink) => l.nav === activeLink;
 
 class XenaNav extends React.Component {
@@ -62,7 +69,8 @@ class XenaNav extends React.Component {
 				<a href='http://xena.ucsc.edu/' className={compStyles.logoXena}><img title={window.ga ? '' : 'no analytics'} src={logoSantaCruzImg} srcSet={logoSrcSet}/></a>
 				<Navigation type="horizontal" routes={routes}>
 					{getState ? <BookmarkMenu isPublic={isPublic} getState={getState} onImport={onImport}/> : null}
-					<Link {...helpLink} />
+					<Link {...geneSetsLink} />
+                    <Link {...helpLink} />
 				</Navigation>
 			</AppBar>
 		);
