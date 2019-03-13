d95fefe1ffb8e4c8d31e04b388d29ca1688b208d
Brian Craft <craft@soe.ucsc.edu>
2019-03-13 14:32:29 -0700

    Drop deprecated composite field joins.
    It's slow on singlecell data.

--- js/models/datasetJoins.js
+++ js/models/datasetJoins.js
@@ -1,14 +1,13 @@
 'use strict';
 var _ = require('underscore');
-var multi = require('../multi');
 var fieldFetch = require('../fieldFetch');
 var Rx = require('../rx');
-var {remapSamples, remapCodes, floatToCoded, concatValuesByFieldPosition,
-		concatMutation, computeMean} = require('./fieldData');
 var samplesFrom = require('../samplesFrom');
 var {nullField} = require('./fieldSpec');
 
 // Strategies for joining field metadata with composite cohorts.
+// XXX All this is deprecated. We need to rework getColSpec
+// and the column add code before dropping the rest of this.
 
 var nonNullFS = fss => fss.filter(fs => fs.fetchType !== 'null');
 
@@ -170,140 +169,9 @@ function getColSpec(fieldSpecs, datasets) {
 	return combineColSpecs(fillNullFields(fieldSpecs), datasets);
 }
 
-// Convert field valueType.
-// (column, fieldSpec, samples, data) => newData
-var cvtField = multi((column, field) => `${field.valueType}->${column.valueType}`, 4);
-
-// For probes, genes, data is
-// req: {values: {[probe]: {sample: value, ...}, ...}}
-cvtField.dflt = (column, fieldSpec, samples, data) => data;
-
-// Column must be single-valued prior to trying to convert to coded.
-cvtField.add('float->coded',
-		(column, fieldSpec, samples, data) => floatToCoded(data));
-
-cvtField.add('null->coded', () => {
-	return {
-		codes: [],
-		req: {
-			values: [],
-			mean: []
-		}
-	};
-});
-
-cvtField.add('null->float', () => {
-	return {
-		req: {
-			values: [],
-			mean: []
-		}
-	};
-});
-
-cvtField.add('null->mutation', () => {
-	return {
-		req: {
-			rows: [],
-			samplesInResp: []
-		}
-	};
-});
-
-cvtField.add('mutation->float', (column, field, samples, acc, data) => {
-	var bySampleID = _.groupBy(data.rows, 'sampleID');
-	return {
-		req: {
-			values: _.map(samples, id => bySampleID[id].length > 0 ? 1 : 0)
-		}
-	};
-});
-
-// We are computing bySample also in the app selector.
-cvtField.add('mutation->coded', (column, field, samples, acc, data) => {
-	var bySampleID = _.groupBy(data.rows, 'sampleID');
-	return {
-		req: {
-			codes: ['no mutation', 'has mutation'],
-			values: _.map(samples, id => bySampleID[id].length > 0 ? 1 : 0)
-		}
-	};
-});
-
-
-function mergeField(field, data, fdata) {
-	var v = _.getIn(fdata, [0, 'req', field]);
-	return v ? _.assocIn(data, ['req', field], v) : data;
-}
-
-// We should only join when the probemap is identical, so the probes for the composite field
-// is the same as any of the constituent fields.
-function setProbes(initData, fdata)  {
-	return ['probes', 'position']
-		.reduce((data, field) => mergeField(field, data, fdata), initData);
-}
-
-// We don't want a reducing function for getField 'mutation'.
-var getField = multi(column => column.valueType);
-
-// Combining float fields:
-// concat all fields by position,
-// copy probe list,
-// compute mean.
-getField.add('float', (column, samplesList, fdata) => {
-	var cvtdData = _.mmap(column.fieldSpecs, samplesList, fdata, cvtField(column)),
-		field = concatValuesByFieldPosition(samplesList, cvtdData);
-
-	return _.assoc(computeMean(setProbes(field, fdata)),
-		'refGene', findFirstProp(fdata, 'refGene'));
-});
-
-// Combining coded fields:
-// find union of codes,
-// assign new values to codes,
-// for each dataset
-//     map val -> code -> new val.
-getField.add('coded', (column, samplesList, fdata) => {
-	var cvtdData = _.mmap(column.fieldSpecs, samplesList, fdata, cvtField(column)),
-		// We reverse prior to taking the union so that codes from the top cohort
-		// will tend to appear on the top of the heatmap (higher values).
-		allCodes = _.union(..._.pluck(_.reverse(cvtdData), 'codes')),
-		mapping = _.object(allCodes, _.range(allCodes.length)),
-		remappedWdata = _.map(cvtdData, remapCodes(mapping));
-	return _.assoc(concatValuesByFieldPosition(samplesList, remappedWdata),
-		'codes', allCodes);
-});
-
-// Combining mutation fields.
-// Require same refGene.
-// Map sampleIDs to their order in cohort samples.
-getField.add('mutation', (column, samples, fdata) => {
-	var sampleOffsets = _.initial(_.scan(samples, (acc, list) => acc + list.length, 0)),
-		sampleMaps = _.map(sampleOffsets, offset => s => s + offset),
-		remappedFdata = _.mmap(sampleMaps, fdata, remapSamples);
-
-	return _.assoc(concatMutation(remappedFdata),
-		'refGene', findFirstProp(fdata, 'refGene'));
-});
-
-// Combining segmented fields.
-// Require same refGene.
-// Map sampleIDs to their order in cohort samples.
-getField.add('segmented', (column, samples, fdata) => {
-	var sampleOffsets = _.initial(_.scan(samples, (acc, list) => acc + list.length, 0)),
-		sampleMaps = _.map(sampleOffsets, offset => s => s + offset),
-		remappedFdata = _.mmap(sampleMaps, fdata, remapSamples);
-
-	return _.assoc(concatMutation(remappedFdata),
-		'refGene', findFirstProp(fdata, 'refGene'));
-});
-
-
 // XXX deprecate this
 function fetchComposite(column, samples) {
-	var {fieldSpecs} = column;
-	return Rx.Observable.zipArray(fieldSpecs.map(f => fieldFetch(f, samples)))
-		.map(fdata => getField(column, samples, fdata));
+	return fieldFetch(column.fieldSpecs[0], samples);
 }
 
 function fetchNull() {
