c8fc695254260b57a23f3ea1b2b95267594b92a5
Brian Craft <craft@soe.ucsc.edu>
2019-05-14 20:18:46 +0000

    probe autosuggest

--- js/xenaQuery.js
+++ js/xenaQuery.js
@@ -324,7 +324,10 @@ function transformPOSTMethods(postMethods) {
 				.map(list => alignMatches(fields, list)),
 		matchFieldsFaster: postFn => (host, dataset, fields) =>
 			postFn(host, dataset, _.flatmap(fields, permuteCase))
-				.map(list => alignMatches(fields, list))
+				.map(list => alignMatches(fields, list)),
+		matchPartialField: postFn => (host, dataset, prefix, limit) =>
+			postFn(host, dataset, prefixPatterns(prefix), limit)
+			.map(filterByPrefix(prefix))
 	};
 
 	var mapPostFn = (transform, name) => transform(postMethods[name]),
@@ -361,6 +364,7 @@ function wrapDsIDParams(postMethods) {
 		'refGeneRange',
 		'matchFields',
 		'matchFieldsFaster',
+		'matchPartialField',
 		'segmentedDataRange',
 		'segmentedDataExamples',
 		'sparseData',
