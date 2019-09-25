4ea23bdb09c05e63ea1ce282064dd37e9c03db29
jingchunzhu <jingchunzhu@gmail.com>
2019-09-25 14:03:42 -0700

    add GRCh37 GRCh38 chrom info

--- js/chromInfo.js
+++ js/chromInfo.js
@@ -2,5 +2,7 @@
 module.exports = {
 	hg18: require('./data/chromInfo_hg18.json'),
 	hg19: require('./data/chromInfo_hg19.json'),
-	hg38: require('./data/chromInfo_hg38.json')
+	GRCh37: require('./data/chromInfo_hg19.json'),
+	hg38: require('./data/chromInfo_hg38.json'),
+	GRCh38: require('./data/chromInfo_hg38.json')
 };
