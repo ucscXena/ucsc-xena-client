2f724ccb5428b08ed800dc4f38b9b82bb6e6abc2
jingchunzhu <jingchunzhu@gmail.com>
2019-12-18 13:10:50 -0800

    when there are no preferred phenotype variables specified, phenotype variable select UI automatically shows all variables options, and there is no switch between basic and "show all" mode. The new behavior is similar to that of the genomic variable select UI.

--- js/views/VariableSelect.js
+++ js/views/VariableSelect.js
@@ -128,7 +128,7 @@ var PhenotypicForm = props => {
 		<div>
 			<XCheckboxGroup
 				label='Phenotype'
-				additionalAction={props.advanced ? 'Show Basic' : 'Show All'}
+				additionalAction={!_.isEmpty(props.preferred) && (props.advanced ? 'Show Basic' : 'Show All')}
 				onAdditionalAction={props.onAdvancedClick}
 				onChange={props.onChange}
 				options={selectedOptions(props.selected, [{options}])}/>
@@ -281,7 +281,7 @@ class VariableSelect extends PureComponent {
 			mode,
 			advanced: {
 				Genotypic: _.isEmpty(preferred),
-				Phenotypic: false,
+				Phenotypic: _.isEmpty(preferred),
 				Analytic: false
 			},
 			basicFeatures: featureIndexes(features, basicFeatures),
