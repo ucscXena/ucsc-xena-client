1a5efbfc027961e2c13d395d53a822898f650895
Brian Craft <bcboy@thecraftstudio.com>
2020-04-09 12:31:53 -0700

    Update to webpack 3.

--- webpack.test.js
+++ webpack.test.js
@@ -1,19 +1,16 @@
-'use strict';
-
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
 var config = require('./webpack.config');
 
 config.output.filename = "testBundle.js";
 config.output.publicPath = "";
-config.entry = {test: 'mocha!./test/all.js'};
+config.entry = {test: 'mocha-loader!./test/all.js'};
 config.plugins = [
 	new HtmlWebpackPlugin({
 		title: "UCSC Xena",
 		filename: "index.html",
-		template: "test.template"
-	}),
-	new webpack.OldWatchingPlugin()
+		inject: false,
+		template: "!!blueimp-tmpl-loader!test.template"
+	})
 ];
 
 module.exports = config;
