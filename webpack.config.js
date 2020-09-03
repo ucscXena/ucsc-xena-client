08d31ace23ea985d5359b453e972237df25bbc76
Brian Craft <bcboy@thecraftstudio.com>
2020-09-03 09:43:52 -0700

    Allow data uri for images.

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
 		disableHostCheck: true,
 		historyApiFallback: true,
 		headers: {
-			"Content-Security-Policy-Report-Only": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src *; img-src *; frame-src 'self' ucscxena: https://www.youtube.com https://xenageneset.berkeleybop.io/xena/"
+			"Content-Security-Policy-Report-Only": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src *; img-src * data:; frame-src 'self' ucscxena: https://www.youtube.com https://xenageneset.berkeleybop.io/xena/"
 		},
 		proxy: {
 			'/api/**': {
