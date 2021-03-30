7503ef813451e5d8139c48caa3225f8254f098d6
Brian Craft <bcboy@thecraftstudio.com>
2021-03-30 15:04:45 -0700

    Include column labels in pdf

--- js/pdfSpreadsheet.js
+++ js/pdfSpreadsheet.js
@@ -23,6 +23,49 @@ function getOffsets(cols) {
 
 var pdfImgHeight = 2000;
 
+var columnLabels = {
+	margin: {v: 16, h: 16},
+	height: 24,
+	dataset: {
+		font: 14,
+		style: '#000000'
+	},
+	field: {
+		font: 14,
+		style: '#707070'
+	}
+};
+
+function txtEllipsis(vg, font, width, txt) {
+	var w = vg.textWidth(font, txt);
+	if (w <= width) {
+		return txt;
+	}
+	for (var i = txt.length - 4; i > 0; --i) {
+		txt = txt.slice(0, i) + '...';
+		w = vg.textWidth(font, txt);
+		if (w <= width) {
+			return txt;
+		}
+	}
+
+	return '';
+}
+
+function drawColumnLabel(vg, column) {
+	var {margin, height, field, dataset} = columnLabels,
+		w = column.width - margin.h * 2,
+		{user: {columnLabel, fieldLabel}} = column;
+	vg.clip(0, 0, w + margin.h, height, () => {
+		vg.text(margin.h, margin.v, dataset.style, dataset.font,
+			txtEllipsis(vg, dataset.font, w, columnLabel));
+	});
+	vg.clip(0, height, w + margin.h, 2 * height, () => {
+		vg.text(margin.h, margin.v + height, field.style, field.font,
+			txtEllipsis(vg, field.font, w, fieldLabel));
+	});
+}
+
 var download = state => {
 	require.ensure(['pdfkit', 'blob-stream', './vgpdf'], () => {
 		var PDFDocument = require('pdfkit');
@@ -31,16 +74,22 @@ var download = state => {
 		let columns = state.columnOrder.map(id => state.columns[id]),
 			data = state.data,
 			width = totalWidth(columns),
+			annotationY = 2 * columnLabels.height,
+			columnY = annotationY + annotationHeight,
 			// pdfkit zlib is pathologically slow.
 			doc = new PDFDocument({compress: false,
-				size: [width, state.zoom.height + annotationHeight]}),
+				size: [width, state.zoom.height + annotationHeight +
+					2 * columnLabels.height]}),
 			stream = doc.pipe(blobStream()),
 			vg = vgpdf(doc, pdfImgHeight * width / state.zoom.height, pdfImgHeight),
 			offsets = getOffsets(columns);
 
 		columns.forEach((column, i) => {
+			vg.translate(offsets[i], 0, () => {
+				drawColumnLabel(vg, column);
+			});
 			if (showPosition(column)) {
-				vg.translate(offsets[i], 0, () => {
+				vg.translate(offsets[i], annotationY, () => {
 					vg.clip(0, 0, column.width, annotationHeight, () =>
 						vg.mirror(vgcanvas(document.createElement('canvas'),
 								column.width, annotationHeight), vgw => {
@@ -58,7 +107,7 @@ var download = state => {
 					}));
 				});
 			}
-			vg.translate(offsets[i], annotationHeight, () => {
+			vg.translate(offsets[i], columnY, () => {
 				widgets.pdf(state.columnOrder[i], column, vg, state, i);
 			});
 		});
