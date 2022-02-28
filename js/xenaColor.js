971dabe6f9a83d3fac093172d0b00c382cc97466
Fran McDade <franmcdade@Frans-MBP-2.hub>
2022-02-28 18:17:47 +1000

    Mui theme restructure. #520.

--- js/xenaColor.js
+++ js/xenaColor.js
@@ -1,9 +1,13 @@
 export const xenaColor = {
 	'ACCENT': '#4cc9c0',
 	'BLACK_12': 'rgba(0, 0, 0, 0.12)',
+	'BLACK_38': 'rgba(0, 0, 0, 0.38)',
+	'BLACK_54': 'rgba(0, 0, 0, 0.54)',
 	'BLACK_87': 'rgba(0, 0, 0, 0.87)',
 	'GRAY': '#eee',
+	'GRAY_LIGHT': '#f7f7f7',
 	'PRIMARY': '#1a535c',
 	'PRIMARY_CONTRAST': '#ff6b6b',
+	'WARNING': '#8d0000',
 	'WHITE': '#ffffff',
 };
