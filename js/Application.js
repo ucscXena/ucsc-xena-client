f78e905479f5bf85100f9bf36bd325212810607f
Fran McDade <franmcdade@MiniMac.local>
2022-06-15 14:20:17 +1000

    Nav Bar disappears if you navigate away from /heatmap/ page. #552.

--- js/Application.js
+++ js/Application.js
@@ -59,6 +59,11 @@ class Application extends PureComponent {
 //			Perf.start();
 //		}
 //	}
+	componentDidMount() {
+		const {onNavigate} = this.props;
+		nav({activeLink: 'heatmap', onNavigate});
+	}
+
 	componentDidUpdate() {
 		const { getState, onImport, onNavigate, state: { isPublic } } = this.props;
 
