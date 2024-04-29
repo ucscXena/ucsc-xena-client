60b8b3f4c2675b45410485a8f4a7edf6ebfc9665
Brian Craft <bcboy@thecraftstudio.com>
2024-04-29 15:13:35 -0700

    auth dialog styling

--- js/Auth.js
+++ js/Auth.js
@@ -3,7 +3,9 @@ import {Button, Dialog, DialogActions, DialogContent, Link}
 
 import {Error} from '@material-ui/icons';
 
-import {el, br} from './chart/react-hyper';
+import {el, br, span} from './chart/react-hyper';
+
+import {hidden} from './nav';
 
 var button = el(Button);
 var dialog = el(Dialog);
@@ -12,9 +14,21 @@ var dialogContent = el(DialogContent);
 var link = el(Link);
 var errorIcon = el(Error);
 
+var errorStyle = {
+	color: 'red',
+	fontSize: '110%',
+	position: 'relative',
+	top: 2
+};
+
+var showCancel;
+var setCancel = next => showCancel = next;
+showCancel = hidden.create('authcancel', 'Auth cancel button', {onChange: setCancel});
+
 export default (host, url, onClick, error) =>
 	dialog({open: true}, dialogContent(
-		...(error ? [errorIcon(), error, br()] : []),
-		`Auth required for ${host}`, br(),
+		...(error ? [span(errorIcon({style: errorStyle}), error, br())] : []),
+		`Authentication required for ${host}`, br(),
 			link({href: url}, 'Log in with Google')),
-		dialogActions(button({onClick: () => onClick(host)}, 'Cancel')));
+		...(showCancel ?
+			[dialogActions(button({onClick: () => onClick(host)}, 'Cancel'))] : []));
