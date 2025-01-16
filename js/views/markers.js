600cf4a6efefdfd91cd7c464030d543865f6491b
Brian Craft <bcboy@thecraftstudio.com>
2025-01-16 10:29:02 -0800

    download marker genes

--- js/views/markers.js
+++ js/views/markers.js
@@ -1,4 +1,4 @@
-var {map} = require('../underscore_ext').default;
+var {extend, map} = require('../underscore_ext').default;
 import {Button, Dialog, DialogActions, DialogContent,
 	DialogTitle} from '@material-ui/core';
 import {el} from '../chart/react-hyper';
@@ -12,10 +12,24 @@ var dialogTitle = el(DialogTitle);
 
 import styles from './markers.module.css';
 
+function download(markers) {
+	var txt = map(markers, (genes, cluster) => [cluster, genes.join(', ')].join('\t'))
+		.join('\n');
+	// use blob for bug in chrome: https://code.google.com/p/chromium/issues/detail?id=373182
+	var url = URL.createObjectURL(new Blob([txt], { type: 'text/tsv' }));
+	var a = document.createElement('a');
+	var filename = 'marker_genes.tsv';
+	extend(a, {id: filename, download: filename, href: url});
+	document.body.appendChild(a);
+	a.click();
+	document.body.removeChild(a);
+}
+
 export default (onClick, {markers, label}) =>
 	dialog({open: true, fullWidth: true, maxWidth: 'md', className: styles.dialog},
 		dialogTitle(`Marker Genes for ${label}`),
 		dialogContent(tableLayout({className: styles.table},
 			['Cluster', 'Marker Genes'],
 			map(markers, (genes, cluster) => [cluster, genes.join(', ')]))),
-		dialogActions(button({onClick}, 'Close')));
+		dialogActions(button({onClick: () => download(markers)}, 'Download'),
+			button({onClick}, 'Close')));
